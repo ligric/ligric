cc0308d6852d8c22f710b54d87374c81147d9678
limeniye <limeniye@gmail.com>
2021-10-14 19:53:10 +0300

    Added Uno platform solution

--- LigricUno/LigricUno/LigricUno.Shared/MainPage.xaml.cs
+++ LigricUno/LigricUno/LigricUno.Shared/MainPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
 namespace LigricUno
 {
     public sealed partial class MainPage : Page
