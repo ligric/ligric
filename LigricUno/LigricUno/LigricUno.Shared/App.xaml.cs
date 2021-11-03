33751f9ab5b58edf704c3db55b3f895f06dde1ab
limeniye <limeniye@gmail.com>
2021-11-03 22:04:27 +0200

    Simple template of Ad Board

--- LigricUno/LigricUno/LigricUno.Shared/App.xaml.cs
+++ LigricUno/LigricUno/LigricUno.Shared/App.xaml.cs
@@ -81,7 +81,7 @@ namespace LigricUno
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(LoginPage), args.Arguments);
+                    rootFrame.Navigate(typeof(BoardsPage), args.Arguments);
                 }
                 // Ensure the current window is active
                 _window.Activate();
---
+++ LigricUno/LigricUno/LigricUno.Shared/Views/Templates/Ad1.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace LigricUno.Views.Templates
+{
+    public sealed class Ad1 : Control
+    {
+        public Ad1()
+        {
+            this.DefaultStyleKey = typeof(Ad1);
+        }
+    }
+}
