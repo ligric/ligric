74f1911e4f2b5b9475de157d4acefa8b7d417769
limeniye <limeniye@gmail.com>
2021-11-07 21:39:16 +0200

    Custom default item

---
+++ LigricUno/LigricUno/LigricUno.Shared/Views/CustomControls/ItemStandard.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LigricUno.Views.CustomControls
+{
+    public sealed class ItemStandard : Control
+    {
+        public string Trader { get => (string)GetValue(TraderProperty); set => SetValue(TraderProperty, value); }
+        public static readonly DependencyProperty TraderProperty = DependencyProperty.Register(nameof(Trader), typeof(string), typeof(ItemStandard), new PropertyMetadata(string.Empty));
+
+
+        public string PaymentMethod { get => (string)GetValue(PaymentMethodProperty); set => SetValue(PaymentMethodProperty, value); }
+        public static readonly DependencyProperty PaymentMethodProperty = DependencyProperty.Register(nameof(PaymentMethod), typeof(string), typeof(ItemStandard), new PropertyMetadata(string.Empty));
+
+
+        public string Rate { get => (string)GetValue(RateProperty); set => SetValue(RateProperty, value); }
+        public static readonly DependencyProperty RateProperty = DependencyProperty.Register(nameof(Rate), typeof(string), typeof(ItemStandard), new PropertyMetadata(string.Empty));
+
+
+        public string Limit { get => (string)GetValue(LimitProperty); set => SetValue(LimitProperty, value); }
+        public static readonly DependencyProperty LimitProperty = DependencyProperty.Register(nameof(Limit), typeof(string), typeof(ItemStandard), new PropertyMetadata(string.Empty));
+        
+
+        public ItemStandard() => this.DefaultStyleKey = typeof(ItemStandard);
+    }
+}
