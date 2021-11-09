cd568f6f547005e798018eeb420d8265cf023a60
limeniye <limeniye@gmail.com>
2021-11-09 22:50:31 +0200

    BoardCustomControl Released

--- LigricUno/LigricUno/LigricUno.Shared/Views/CustomControls/ItemStandard.cs
+++ LigricUno/LigricUno/LigricUno.Shared/Views/CustomControls/ItemStandard.cs
@@ -1,10 +1,30 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace LigricUno.Views.CustomControls
 {
     public sealed class ItemStandard : Control
     {
+        public IDictionary<string, string> Properties { get => (IDictionary<string, string>)GetValue(PropertiesProperty); set => SetValue(PropertiesProperty, value); }
+        public static readonly DependencyProperty PropertiesProperty = DependencyProperty.Register(nameof(Properties), typeof(IDictionary<string, string>), typeof(ItemStandard), new PropertyMetadata(new Dictionary<string, string>(), OnPropertiesChanged));
+
+        private static void OnPropertiesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var dict = (IDictionary<string, string>)e.NewValue;
+
+            if (dict.TryGetValue("Trader", out string trader))
+                ((ItemStandard)d).Trader = trader;
+            if (dict.TryGetValue("PaymentMethod", out string paymentMethod))
+                ((ItemStandard)d).PaymentMethod = paymentMethod;
+            if (dict.TryGetValue("Rate", out string rate))
+                ((ItemStandard)d).Rate = rate;
+            if (dict.TryGetValue("Limit", out string limit))
+                ((ItemStandard)d).Limit = limit;
+            //throw new NotImplementedException();
+        }
+
         public string Trader { get => (string)GetValue(TraderProperty); set => SetValue(TraderProperty, value); }
         public static readonly DependencyProperty TraderProperty = DependencyProperty.Register(nameof(Trader), typeof(string), typeof(ItemStandard), new PropertyMetadata(string.Empty));
 
---
+++ LigricUno/LigricUno/LigricUno.Shared/Views/Pages/BoardsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using LigricMvvm.BaseMvvm;
+using System.Collections.ObjectModel;
+
+namespace LigricUno.Views.Pages
+{
+    public class AdViewModel : OnNotifyPropertyChanged
+    {
+        private int _id = 0;
+        private string _trader, _paymentMethod, _rate, _limit;
+        
+        public int Id { get => _id; private set => SetProperty(ref _id, value); }
+        public string Trader { get => _trader; set => SetProperty(ref _trader, value); }
+        public string PaymentMethod { get => _paymentMethod; set => SetProperty(ref _paymentMethod, value); }
+        public string Rate { get => _rate; set => SetProperty(ref _rate, value); }
+        public string Limit { get => _limit; set => SetProperty(ref _limit, value); }
+    }
+
+
+    public class BoardViewModel : OnNotifyPropertyChanged
+    {
+        private int _id = 0;
+        private string _title = "BitZlato";
+
+
+        public int Id { get => _id; private set => SetProperty(ref _id, value); }
+        public string Title { get => _title; set => SetProperty(ref _title, value); }
+
+        private ObservableCollection<AdViewModel> _ads = new ObservableCollection<AdViewModel>()
+        {
+            new AdViewModel(){ Trader = "limeniye",  PaymentMethod = "Альфа Банк", Rate = "9 999 999" , Limit = "100 000 - 140 130" }
+        };
+
+        public ObservableCollection<AdViewModel> Ads { get => _ads; set => SetProperty(ref _ads, value); }
+    }
+
+    public class BoardsViewModel : OnNotifyPropertyChanged
+    {
+        private BoardViewModel _testBoard = new BoardViewModel();
+        public BoardViewModel TestBoard { get => _testBoard; set => SetProperty(ref _testBoard, value); }
+
+        public ObservableCollection<BoardViewModel> Boards = new ObservableCollection<BoardViewModel>();
+    }
+}
