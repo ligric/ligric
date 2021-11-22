73c15696e8509aefd96e638863bd43deef16acf6
limeniye <limeniye@gmail.com>
2021-11-22 19:57:45 +0200

    DragPositionBehavior Fixed by @EldHasp

--- LigricView/ViewModel/LigricMvvmToolkit/Behaviors/DragPositionBehavior.cs
+++ LigricView/ViewModel/LigricMvvmToolkit/Behaviors/DragPositionBehavior.cs
@@ -1,11 +1,9 @@
-﻿using Common;
-using Microsoft.Xaml.Interactivity;
+﻿using Microsoft.Xaml.Interactivity;
 using System;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Input;
 using Windows.Foundation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 
@@ -13,11 +11,15 @@ namespace LigricMvvmToolkit.Behaviors
 {
     public partial class DragPositionBehavior : DependencyObject, IBehavior
     {
-        public DependencyObject AssociatedObject{ get; set; }
+        public DependencyObject AssociatedObject { get; set; }
+        public UIElement AssociatedUIElement { get; private set; }
+
 
         private Point prevPoint;
         private int pointerId = -1;
 
+        public static readonly DependencyProperty TransformProperty = DependencyProperty.RegisterAttached(nameof(Transform), typeof(Transform), typeof(DragPositionBehavior), new PropertyMetadata(null));
+        public Transform Transform { get => (Transform)GetValue(TransformProperty); set => SetValue(TransformProperty, value); }
 
         public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(DragPositionBehavior), new PropertyMetadata(null));
         public ICommand Command { get => (ICommand)GetValue(CommandProperty); set => SetValue(CommandProperty, value); }
@@ -26,89 +28,90 @@ namespace LigricMvvmToolkit.Behaviors
         public static readonly DependencyProperty ZoomFactorProperty = DependencyProperty.Register(nameof(ZoomFactor), typeof(double), typeof(DragPositionBehavior), new PropertyMetadata(0));
         public double ZoomFactor { get => (double)GetValue(ZoomFactorProperty); set => SetValue(ZoomFactorProperty, value); }
 
-       
-        public static readonly DependencyProperty BaseElementProperty = DependencyProperty.Register(nameof(BaseElement), typeof(UIElement), typeof(DragPositionBehavior), new PropertyMetadata(null));
-        public UIElement BaseElement { get => (UIElement)GetValue(BaseElementProperty); set => SetValue(BaseElementProperty, value); }
+
+        public static readonly DependencyProperty BaseParentProperty = DependencyProperty.Register(nameof(BaseParent), typeof(UIElement), typeof(DragPositionBehavior), new PropertyMetadata(null));
+        public UIElement BaseParent { get => (UIElement)GetValue(BaseParentProperty); set => SetValue(BaseParentProperty, value); }
 
 
         #region Life circle
         public void Attach(DependencyObject associatedObject)
         {
-            if ((associatedObject != AssociatedObject) && !Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                return;
+            }
+
+            if (!(associatedObject is UIElement associatedUIElement))
+            {
+                throw new ArgumentException($"Type: {nameof(DragPositionBehavior)};\nMethod: {nameof(Attach)}\nProblem: \"associatedObject is not UIElement associatedUIElement\".", nameof(associatedObject));
+            }
+
+            if (associatedObject != AssociatedObject)
             {
                 AssociatedObject = associatedObject;
-                
-                var element = AssociatedObject as FrameworkElement;
-                if (element != null)
-                {
-                    BaseElement.PointerPressed += OnElementPointerPressed;
-                    BaseElement.PointerReleased += OnElementPointerReleased;
-                }
+                AssociatedUIElement = associatedUIElement;
+
+                associatedUIElement.PointerPressed += OnElementPointerPressed;
             }
         }
 
         public void Detach()
         {
-            var element = AssociatedObject as FrameworkElement;
-
-            if (BaseElement is not null)
-            {
-                BaseElement.PointerPressed -= OnElementPointerPressed;
-                BaseElement.PointerReleased -= OnElementPointerReleased;
-                BaseElement.PointerMoved -= OnMove;
-            }
-
-            BaseElement = null;
+            AssociatedUIElement.PointerPressed -= OnElementPointerPressed;
             AssociatedObject = null;
+            AssociatedUIElement = null;
         }
         #endregion
 
         #region Handle pointer input
         private void OnElementPointerPressed(object sender, PointerRoutedEventArgs e)
         {
-            var element = AssociatedObject as FrameworkElement;
-
-            if (element == null)
+            if (BaseParent == null)
                 return;
 
-            if (!(element.RenderTransform is TranslateTransform))
-                element.RenderTransform = new TranslateTransform();
-            
-            BaseElement.PointerMoved += OnMove;
+            BaseParent.PointerReleased += OnElementPointerReleased;
 
-            prevPoint = e.GetCurrentPoint(BaseElement).Position;
+            // TODO : Дописать событие выхода за пределы панели.
+            // TODO : Это очень криво. Может у элемента есть своя друга трансформация?
+            // TODO : Это нужно ОБЯЗАТЕЛЬНО заменить на AP-свойства.
+
+            if (!(AssociatedUIElement.RenderTransform is TranslateTransform))
+                AssociatedUIElement.RenderTransform = new TranslateTransform();
+
+            BaseParent.PointerMoved += OnMove;
+
+            prevPoint = e.GetCurrentPoint(BaseParent).Position;
             pointerId = (int)e.Pointer.PointerId;
         }
 
         private void OnElementPointerReleased(object sender, PointerRoutedEventArgs e)
         {
+            var basePanel = (UIElement)sender;
+            basePanel.PointerReleased -= OnElementPointerReleased;
+            basePanel.PointerMoved -= OnMove;
+
             if (e.Pointer.PointerId != pointerId)
                 return;
 
-            var element = AssociatedObject as FrameworkElement;
-            if (element == null)
+            if (AssociatedUIElement == null)
                 return;
 
-            BaseElement.PointerMoved -= OnMove;
             pointerId = -1;
-
-            Point position = e.GetCurrentPoint(element).Position;
-
-            if (Command.CanExecute(position))
-                Command.Execute(position);
+            //Point position = e.GetCurrentPoint(element).Position;
+            //if (Command.CanExecute(position))
+            //    Command.Execute(position);
         }
 
-        private void OnMove(object o, PointerRoutedEventArgs e)
+        private void OnMove(object sender, PointerRoutedEventArgs e)
         {
             var zommFactor = ZoomFactor;
-            var element = AssociatedObject as FrameworkElement;
 
-            if (e.Pointer.PointerId != pointerId || element is null)
+            if (AssociatedUIElement is null)
                 return;
 
-            var pos = e.GetCurrentPoint(BaseElement).Position;
-            ((TranslateTransform)element.RenderTransform).X += (pos.X - prevPoint.X) / zommFactor;
-            ((TranslateTransform)element.RenderTransform).Y += (pos.Y - prevPoint.Y) / zommFactor;
+            var pos = e.GetCurrentPoint(BaseParent).Position;
+            ((TranslateTransform)AssociatedUIElement.RenderTransform).X += (pos.X - prevPoint.X) / zommFactor;
+            ((TranslateTransform)AssociatedUIElement.RenderTransform).Y += (pos.Y - prevPoint.Y) / zommFactor;
             prevPoint = pos;
         }
         #endregion
