5b83486b49194f5b92baf86409763efcc27586a9
limeniye <limeniye@gmail.com>
2022-01-27 20:30:53 +0200

    - Vertical bottom animation works;

--- LigricView/CustomControls/LigricBoardCustomControls/Menus/SnakeMenu.cs
+++ LigricView/CustomControls/LigricBoardCustomControls/Menus/SnakeMenu.cs
@@ -15,7 +15,9 @@ namespace LigricBoardCustomControls.Menus
         CollapsedFromTopToBottom,
         CollapsedFromRightToLeft
     }
-    
+
+    public delegate void ExpanderStateChangedEventArgs(object sender, SnakeExpandDirection newState);
+
     public partial class SnakeMenu : ContentControl
     {
         private const string SNAKE_BACKGROUND_ELEMENT = "SnakeBackgroundElement";
@@ -28,12 +30,9 @@ namespace LigricBoardCustomControls.Menus
         protected Border snakeBackgroundElement;
         protected Border expanderContainer;
         protected ContentControl expanderContent;
-
         protected Border headerConteiner;
         protected Panel headerContent;
         protected ToggleButton toggleButton;
-
-
 
         private SerialDisposable _eventSubscriptions = new SerialDisposable();
 
@@ -45,16 +44,24 @@ namespace LigricBoardCustomControls.Menus
         public static DependencyProperty ExpandDirectionProperty
         {
             get;
-        } = DependencyProperty.Register("ExpandDirection", typeof(SnakeExpandDirection), typeof(SnakeMenu), 
-                new PropertyMetadata(SnakeExpandDirection.CollapsedFromTopToBottom/*SnakeExpandDirection.CollapsedFromTopToBottom*/, propertyChangedCallback: OnExpandDirectionChanged));
+        } = DependencyProperty.Register("ExpandDirection", typeof(SnakeExpandDirection), typeof(SnakeMenu),
+                new PropertyMetadata(SnakeExpandDirection.CollapsedFromTopToBottom, propertyChangedCallback: OnExpandDirectionChanged));
 
-        private static void OnExpandDirectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+
+        public bool IsExpanded
         {
-            var thisObject = (SnakeMenu)d;
-            if (thisObject == null)
-                return;
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+        public static DependencyProperty IsExpandedProperty
+        {
+            get;
+        } = DependencyProperty.Register("IsExpanded", typeof(bool), typeof(SnakeMenu),
+                new PropertyMetadata(defaultValue: false, propertyChangedCallback: OnIsExpandedChanged));
 
-            thisObject.UpdateSnakeExpandDirectionState(true);
+        public SnakeMenu()
+        {
+            base.DefaultStyleKey = typeof(SnakeMenu);
         }
 
         public object Header
@@ -66,6 +73,8 @@ namespace LigricBoardCustomControls.Menus
         { 
             get; 
         } = DependencyProperty.Register("Header", typeof(object), typeof(SnakeMenu), new PropertyMetadata(null));
+
+        public event ExpanderStateChangedEventArgs ExpanderStateChanged;
 
         protected override void OnApplyTemplate()
         {
@@ -80,17 +89,40 @@ namespace LigricBoardCustomControls.Menus
             headerConteiner = GetTemplateChild(HEADER_CONTEINER) as Border;
             headerContent = GetTemplateChild(HEADER_CONTENT) as Panel;
             toggleButton = GetTemplateChild(TOGGLE_BUTTON) as ToggleButton;
-
+            if (toggleButton != null)
+            {
+                //toggleButton.Checked += OnToggleButtonChecked;
+                //toggleButton.Unchecked += OnToggleButtonUnchecked;
+                //disposables.Add(delegate
+                //{
+                //    toggleButton.Checked -= OnToggleButtonChecked;
+                //    toggleButton.Unchecked -= OnToggleButtonUnchecked;
+                //});
+            }
 
             UpdateSnakeExpandDirectionState(false);
             //_eventSubscriptions.Disposable = disposable;
         }
 
+        //private void OnToggleButtonChecked(object sender, RoutedEventArgs e)
+        //{
+        //    ExpanderStateChanged?.Invoke(this, this.ExpandDirection);
+        //}
+
+        //private void OnToggleButtonUnchecked(object sender, RoutedEventArgs e)
+        //{
+        //    ExpanderStateChanged?.Invoke(this, this.ExpandDirection);
+        //}
+
         private void UpdateSnakeExpandDirectionState(bool useTransitions)
         {
             var snakeExpandDirection = ExpandDirection;
+            SetNewSnakeExpandDirectionState(useTransitions, snakeExpandDirection);
+        }
 
-            switch(snakeExpandDirection)
+        private void SetNewSnakeExpandDirectionState(bool useTransitions, SnakeExpandDirection snakeExpandDirection)
+        {
+            switch (snakeExpandDirection)
             {
                 ////// Set vertical visual state
                 case SnakeExpandDirection.CollapsedFromTopToBottom:
@@ -113,6 +145,55 @@ namespace LigricBoardCustomControls.Menus
             };
         }
 
+        private static void OnExpandDirectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var thisObject = (SnakeMenu)d;
+            if (thisObject == null)
+                return;
+
+            thisObject.UpdateSnakeExpandDirectionState(true);
+        }
+
+
+        private static void OnIsExpandedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            bool newValue = (bool)e.NewValue;
+
+            var thisObject = (SnakeMenu)d;
+            if (thisObject == null)
+                return;
+
+            thisObject.IsExpandedUpdated(newValue);
+        }
+
+        private void IsExpandedUpdated(bool expanded)
+        {
+            var stringSnakeExpandDirection = ExpandDirection.ToString();
+
+            if (stringSnakeExpandDirection.Contains("Left") && stringSnakeExpandDirection.Contains("Right"))
+            {
+                if (expanded)
+                {
+                    SetNewSnakeExpandDirectionState(true, SnakeExpandDirection.ExpandedFromLeftToRight);
+                }
+                else
+                {
+                    SetNewSnakeExpandDirectionState(true, SnakeExpandDirection.CollapsedFromRightToLeft);
+                }
+                
+            }
+            else
+            {
+                if (expanded)
+                {
+                    SetNewSnakeExpandDirectionState(true, SnakeExpandDirection.ExpandedFromBottomToTop);
+                }
+                else
+                {
+                    SetNewSnakeExpandDirectionState(true, SnakeExpandDirection.CollapsedFromTopToBottom);
+                }
+            }
+        }
 #if NET6_0_ANDROID
         /// <summary>
         /// Its needed for Android. Native constructor, do not use explicitly.
