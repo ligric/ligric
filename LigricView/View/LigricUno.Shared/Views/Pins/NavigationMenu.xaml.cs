3321fc9f4609a366cde5313e305d01d7435720b9
limeniye <limeniye@gmail.com>
2022-02-14 22:02:38 +0200

    - [Menu] User image is visible;

--- LigricView/View/LigricUno.Shared/Views/Pins/NavigationMenu.xaml.cs
+++ LigricView/View/LigricUno.Shared/Views/Pins/NavigationMenu.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Shapes;
 
 namespace LigricUno.Shared.Views.Pins
 {
@@ -11,18 +13,33 @@ namespace LigricUno.Shared.Views.Pins
 
             //////// TEMPRARY 
             menu.ExpanderSideChanged += OnMenuSideChanged;
+            SetSideSettings(menu.ExpanderSide);
         }
 
         //////// TEMPRARY 
         private void OnMenuSideChanged(object sender, LigricBoardCustomControls.Menus.ExpanderSide newSide)
         {
+            SetSideSettings(newSide);
+        }
+
+        private void SetSideSettings(LigricBoardCustomControls.Menus.ExpanderSide newSide)
+        {
+            Binding binding = new Binding();
+            binding.ElementName = "stackPanel"; // элемент-источник
+
             if (newSide == LigricBoardCustomControls.Menus.ExpanderSide.Left)
             {
                 VisualStateManager.GoToState(this, "ExpanderSettingsForLeftSidee", false);
+
+                binding.Path = new PropertyPath("ActualWidth");
+                userImage.SetBinding(Ellipse.HeightProperty, binding);
             }
             else
             {
-                //VisualStateManager.GoToState(menu, "ExpanderSettingsForBottomSidee", false);
+                VisualStateManager.GoToState(menu, "ExpanderSettingsForBottomSidee", false);
+
+                binding.Path = new PropertyPath("ActualHeight");
+                userImage.SetBinding(Ellipse.WidthProperty, binding);
             }
         }
 
