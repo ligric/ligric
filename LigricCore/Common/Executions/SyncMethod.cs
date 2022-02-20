7bd24c9de0b56fef45b698da1b8f1f0bcdd8cc7f
limeniye <limeniye@gmail.com>
2022-02-20 20:52:39 +0200

    - Added sync method with Task;

--- LigricCore/Common/Executions/SyncMethod.cs
+++ LigricCore/Common/Executions/SyncMethod.cs
@@ -7,12 +7,41 @@ namespace Common
     {
         protected int oldNumber = -1;
 
-        public async Task WaitingAnotherMethodsAsync(int number, Func<Task> action, int milliseconds = 10000)
+        public async void WaitingAnotherMethodsAsync(int number, Action action, int millisecondsLimit = 10000)
         {
             int timeout = 0;
             int еxpectedNumber = number - 1;
 
-            while (oldNumber < еxpectedNumber && timeout < milliseconds)
+            while (oldNumber < еxpectedNumber && timeout < millisecondsLimit)
+            {
+                timeout++;
+                await Task.Delay(1);
+            }
+
+            if (oldNumber < еxpectedNumber)
+            {
+                throw new ArgumentException($"Error message: \"Message after the {oldNumber} was lost.\"");
+            }
+            else if (oldNumber > еxpectedNumber)
+            {
+                throw new ArgumentException($"Error message: \"Message after the {oldNumber} already finished.\"");
+            }
+            else
+            {
+                action();
+
+                oldNumber++;
+                if (oldNumber != number)
+                    throw new ArgumentException("Error message: \"Something wrong ;(((.");
+            }
+        }
+
+        public async void WaitingAnotherMethodsAsync(int number, Func<Task> action, int millisecondsLimit = 10000)
+        {
+            int timeout = 0;
+            int еxpectedNumber = number - 1;
+
+            while (oldNumber < еxpectedNumber && timeout < millisecondsLimit)
             {
                 timeout++;
                 await Task.Delay(1);
