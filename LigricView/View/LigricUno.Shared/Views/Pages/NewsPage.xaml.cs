998cda80e002573d5de6f6585537c1dfed843198
limeniye <limeniye@gmail.com>
2022-04-11 13:45:10 +0300

    - Added empty NewsPage;

---
+++ LigricView/View/LigricUno.Shared/Views/Pages/NewsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace LigricUno.Shared.Views.Pages
+{
+    public sealed partial class NewsPage : Page
+    {
+        public NewsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ LigricView/View/LigricUno.Shared/Views/Pages/NewsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LigricUno.Shared.Views.Pages
+{
+    public class NewsViewModel
+    {
+    }
+}
