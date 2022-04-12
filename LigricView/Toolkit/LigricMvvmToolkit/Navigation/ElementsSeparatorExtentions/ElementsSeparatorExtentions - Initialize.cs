c3b30372b988c02a50b918cf16236d954205bfea
limeniye <limeniye@gmail.com>
2022-04-12 21:03:27 +0300

    - Removed navigation files;

--- LigricView/Toolkit/LigricMvvmToolkit/Navigation/ElementsSeparatorExtentions/ElementsSeparatorExtentions - Initialize.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace LigricMvvmToolkit.Navigation
-{
-    internal static partial class ElementsSeparatorExtensions
-    {
-        private readonly static Dictionary<string, WrapperInfo> wrappers = new Dictionary<string, WrapperInfo>();
-    }
-}
