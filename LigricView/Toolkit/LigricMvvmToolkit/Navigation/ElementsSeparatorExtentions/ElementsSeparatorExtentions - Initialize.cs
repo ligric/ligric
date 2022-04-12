8eb42b405eacf83ad72df6e0b5e811b84ef2943f
limeniye <limeniye@gmail.com>
2022-04-12 20:10:58 +0300

    - Added wrapper info;

--- LigricView/Toolkit/LigricMvvmToolkit/Navigation/ElementsSeparatorExtentions/ElementsSeparatorExtentions - Initialize.cs
+++ LigricView/Toolkit/LigricMvvmToolkit/Navigation/ElementsSeparatorExtentions/ElementsSeparatorExtentions - Initialize.cs
@@ -1,28 +1,9 @@
 ﻿using System.Collections.Generic;
-using System;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml;
 
 namespace LigricMvvmToolkit.Navigation
 {
     internal static partial class ElementsSeparatorExtensions
     {
-        private class WrapperInfo
-        {
-            public string Key { get; }
-
-            public Panel Wrapper { get; }
-
-            public IList<PinInfo> Pins { get; }
-
-            public WrapperInfo(string key, Panel wrapper, IList<PinInfo> pins)
-            {
-                Key = key ?? throw new ArgumentNullException("Key is null.");  
-                Wrapper = wrapper ?? throw new ArgumentNullException("Wrapper is null.");
-                Pins = pins ?? new List<PinInfo>();
-            }
-        }
-
         private readonly static Dictionary<string, WrapperInfo> wrappers = new Dictionary<string, WrapperInfo>();
     }
 }
---
+++ LigricView/Toolkit/LigricMvvmToolkit/Navigation/WrapperInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LigricMvvmToolkit.Navigation
+{
+    public class WrapperInfo
+    {
+        public string Key { get; }
+
+        public object Wrapper { get; }
+
+        public WrapperInfo(string key, object wrapper)
+        {
+            Key = key ?? throw new ArgumentNullException("Wrapper key is null.");
+            Wrapper = wrapper ?? throw new ArgumentNullException("Wrapper is null.");
+        }
+    }
+}
