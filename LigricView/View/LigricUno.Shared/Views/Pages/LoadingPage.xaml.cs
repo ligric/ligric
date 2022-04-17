c368b4a767275967340a4c0e87b403a3c96039a5
limeniye <limeniye@gmail.com>
2022-04-17 17:48:58 +0300

    - Added LoadingPage;

---
+++ LigricView/View/LigricUno.Shared/Views/Pages/LoadingPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace LigricUno.Views.Pages
+{
+    public sealed partial class LoadingPage : Page
+    {
+        public LoadingPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
