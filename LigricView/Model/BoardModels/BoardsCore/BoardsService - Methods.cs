ad6555ac51ad485d45611e7ce88d44acebb3d22a
limeniye <limeniye@gamanet.com>
2022-04-25 14:58:29 +0300

    - Added BoardService - Methods;

---
+++ LigricView/Model/BoardModels/BoardsCore/BoardsService - Methods.cs
@@ -0,0 +1,20 @@
+﻿#if NETSTANDARD
+using Common.Extensions;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BoardsShared.BoardsCore
+{
+    public sealed partial class BoardsService
+    {
+        public async Task AddNewBoard(string key, string value)
+        {
+            if (!boards.TryAdd(key, value))
+                throw new ArgumentException($"Unable to add {key} key to dictionary.");
+
+
+        }
+    }
+}
