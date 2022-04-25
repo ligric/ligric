7ba85e5926c9129e54527e121888d3d828360a4b
limeniye <limeniye@gamanet.com>
2022-04-25 12:55:58 +0300

    - Added MessagePage;

---
+++ LigricView/View/LigricUno.Shared/Views/Pages/Messages/MessagesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace LigricUno.Views.Pages.Messages
+{
+    public sealed partial class MessagesPage : Page
+    {
+        public MessagesPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
