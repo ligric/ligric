697a3ecf3ac68f4f81d1b1c3b94023c14b499c7f
limeniye <limeniye@gamanet.com>
2022-04-25 14:47:53 +0300

    - Added BoardsService - Collections;

---
+++ LigricView/Model/BoardModels/BoardsCore/BoardsService - Collections.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BoardsShared.BoardsCore
+{
+    public sealed partial class BoardsService
+    {
+        private readonly Dictionary<string, string> boards = new Dictionary<string, string>();
+        public IReadOnlyDictionary<string, string> Boards { get; } = new Dictionary<string, string>();
+
+        public BoardsService()
+        {
+            Boards = new ReadOnlyDictionary<string, string>(boards);
+        }
+    }
+}
