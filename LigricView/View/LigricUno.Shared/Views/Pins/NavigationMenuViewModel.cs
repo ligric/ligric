af43e97043c485773dd4154fbf53f6208b65243b
limeniye <limeniye@gamanet.com>
2022-04-27 17:05:31 +0300

    - [Rename] SelectNavigationItemCommand to SelectContentNavigationItemCommand;

--- LigricView/View/LigricUno.Shared/Views/Pins/NavigationMenuViewModel.cs
+++ LigricView/View/LigricUno.Shared/Views/Pins/NavigationMenuViewModel.cs
@@ -1,39 +1,88 @@
-﻿using LigricMvvmToolkit.BaseMvvm;
+﻿using BoardsShared.Abstractions.BoardsAbstractions.Interfaces;
+using LigricMvvmToolkit.BaseMvvm;
 using LigricMvvmToolkit.Navigation;
 using LigricMvvmToolkit.RelayCommand;
-using LigricUno.Views.Pages.Boards;
-using LigricUno.Views.Pages.News;
-using LigricUno.Views.Pages.Profile;
-using System;
+using LigricUno.Views.Pages.Board;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace LigricUno.Views.Pins
 {
     public class NavigationMenuViewModel : DispatchedBindableBase
     {
         private string _selectedContentItem;
+        private RelayCommand<string> _selectHeaderNavigationItemCommand;
+        private RelayCommand<byte> _selectContentNavigationItemCommand;
+
 
         public string SelectedContentItem { get => _selectedContentItem; set => SetProperty(ref _selectedContentItem, value); }
 
         public ObservableCollection<string> HeaderItems { get; } = new ObservableCollection<string>() { "News", "Profile", "Messages", "Settings" };
-        public ObservableCollection<string> ContentItems { get; } = new ObservableCollection<string>() { "Board0", "Board1", "Board2", "Board4", "Board0", "Board1", "Board2", "Board4", "Board0", "Board1", "Board2", "Board4" };
+        public ObservableCollection<byte> ContentItems { get; } = new ObservableCollection<byte>();
 
 
-        private RelayCommand<string> _selectNavigationItemCommand;
-        public RelayCommand<string> SelectNavigationItemCommand => _selectNavigationItemCommand ?? (_selectNavigationItemCommand = new RelayCommand<string>(OnSelectedExecute, CanSelectedExecute));
+        public RelayCommand<string> SelectHeaderNavigationItemCommand => _selectHeaderNavigationItemCommand ?? 
+            (_selectHeaderNavigationItemCommand = 
+                new RelayCommand<string>(OnSelectedHeaderExecute, CanSelectedHeaderExecute));
 
-        private void OnSelectedExecute(string parameter)
+        private void OnSelectedHeaderExecute(string parameter)
         {
-            Navigation.GoTo(parameter + "Page");
+            Navigation.GoTo(parameter + "Page");           
         }
-        private bool CanSelectedExecute(string parameter)
+
+        private bool CanSelectedHeaderExecute(string parameter)
         {
             return !Navigation.GetCurrentPageKey().Contains(parameter + "Page");
         }
 
+        public RelayCommand<byte> SelectContentNavigationItemCommand => _selectContentNavigationItemCommand ??
+            (_selectContentNavigationItemCommand =
+                new RelayCommand<byte>(OnSelectedContentExecute, CanSelectedContentExecute));
+
+
+        private void OnSelectedContentExecute(byte parameter)
+        {
+            if (!Navigation.GetCurrentPageKey().Contains(nameof(BoardPage)))
+            {
+                Navigation.GoTo(nameof(BoardPage));
+            }
+        }
+        private bool CanSelectedContentExecute(byte parameter)
+        {
+            return true;
+        }
+
+
+
+
         public NavigationMenuViewModel()
         {
-            Navigation.PageChanged += (pageKey) => SelectNavigationItemCommand.RaiseCanExecuteChanged();
+            Navigation.PageChanged += (pageKey) => SelectHeaderNavigationItemCommand.RaiseCanExecuteChanged();
+
+            var boardsService = IocService.ServiceProvider.GetService<IBoardsService>();
+            boardsService.BoardsChanged += OnBoardsChanged;
+            boardsService.AddBoard();
+        }
+
+        private void OnBoardsChanged(object sender, Common.EventArgs.NotifyDictionaryChangedEventArgs<byte, BoardsShared.CommonTypes.Entities.Board.BoardDto> e)
+        {
+            switch (e.Action)
+            {
+                case Common.EventArgs.NotifyDictionaryChangedAction.Added:
+                    ContentItems.Add(e.NewValue.Id);
+                    break;
+                case Common.EventArgs.NotifyDictionaryChangedAction.Removed:
+                    break;
+                case Common.EventArgs.NotifyDictionaryChangedAction.Changed:
+                    break;
+                case Common.EventArgs.NotifyDictionaryChangedAction.Cleared:
+                    break;
+                case Common.EventArgs.NotifyDictionaryChangedAction.Initialized:
+                    break;
+                default:
+                    break;
+            }
         }
 
         protected override void OnPropertyChanged(string propertyName, object oldValue, object newValue)
@@ -45,9 +94,9 @@ namespace LigricUno.Views.Pins
                 var newValueString = newValue as string;
                 if (newValueString != null)
                 {
-                    if (newValueString.Contains(nameof(BoardsPage)))
+                    if (newValueString.Contains(nameof(BoardPage)))
                     {
-                        Navigation.GoTo(nameof(BoardsPage));
+                        Navigation.GoTo(nameof(BoardPage));
                     }
                 }
             }
