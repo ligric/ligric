11cfec3bcc88736fcad9bbc516fa4130e06da490
limeniye <limeniye@gamanet.com>
2022-05-01 23:25:38 +0300

    - Added NavigationItemsService;

---
+++ LigricView/View/LigricUno.Shared/LocalViewServices/NavigationItemsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LigricUno.LocalViewServices
+{
+    internal static class NavigationItemsService
+    {
+        public static event Action<string> ItemSelected;
+
+        public static void RaiseItemSelectedAction(string itemKey)
+            => ItemSelected?.Invoke(itemKey);
+    }
+}
