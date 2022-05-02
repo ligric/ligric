ff393cd6286e7ca69c12b551223841674b34b217
limeniye <limeniye@gamanet.com>
2022-05-02 23:42:03 +0300

    - Created SuctionCup;

---
+++ LigricView/CustomControls/LigricBoardCustomControls/Menus/SuctionCup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace LigricBoardCustomControls.Menus
+{
+    public partial class SuctionCup : ToggleButton
+    {
+        public SuctionCup()
+        {
+            base.DefaultStyleKey = typeof(SuctionCup);
+        }
+    }
+}
