72725ba4940457d54e1a7d26019d5462693679b8
limeniye <limeniye@gamanet.com>
2022-05-03 01:13:30 +0300

    - Added wave line for SuctionCup;

--- LigricView/CustomControls/LigricBoardCustomControls/Menus/SuctionCup.cs
+++ LigricView/CustomControls/LigricBoardCustomControls/Menus/SuctionCup.cs
@@ -6,6 +6,18 @@ namespace LigricBoardCustomControls.Menus
 {
     public partial class SuctionCup : ToggleButton
     {
+        public object PopupContent
+        {
+            get { return GetValue(PopupContentProperty); }
+            set { SetValue(PopupContentProperty, value); }
+        }
+        public static DependencyProperty PopupContentProperty
+        {
+            get;
+        } = DependencyProperty.Register(nameof(PopupContent), typeof(object), typeof(SuctionCup), new PropertyMetadata(null, null));
+
+
+
         public SuctionCup()
         {
             base.DefaultStyleKey = typeof(SuctionCup);
