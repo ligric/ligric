25e33f6f54f0547c197d684344f2ab3815db2bb3
limeniye <limeniye@gamanet.com>
2022-05-02 23:00:48 +0300

    - Hot fixes;

--- LigricView/View/LigricUno.Shared/App.xaml.cs
+++ LigricView/View/LigricUno.Shared/App.xaml.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
-using Windows.UI;S
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
