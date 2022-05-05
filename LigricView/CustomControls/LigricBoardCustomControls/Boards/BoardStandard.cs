3d3a5e3c2e59e5f957ff9b0e86e43335956a8ad2
limeniye <limeniye@gamanet.com>
2022-05-05 09:46:13 +0300

    - Added contents for BoardStandard;

--- LigricView/CustomControls/LigricBoardCustomControls/Boards/BoardStandard.cs
+++ LigricView/CustomControls/LigricBoardCustomControls/Boards/BoardStandard.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace LigricBoardCustomControls.Boards
 {
-    public partial class BoardStandard : Control
+    public partial class BoardStandard : ContentControl
     {
         public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(BoardStandard), new PropertyMetadata(string.Empty));
 
-        public IList<IDictionary<string,string>> Ads { get => (IList<IDictionary<string, string>>)GetValue(AdsProperty); set => SetValue(AdsProperty, value); }
-        public static readonly DependencyProperty AdsProperty = DependencyProperty.Register(nameof(Ads), typeof(IList<IDictionary<string, string>>), typeof(BoardStandard), new
-            PropertyMetadata(new List<IDictionary<string, string>>()));
+        public object FiltersContent
+        {
+            get { return GetValue(FiltersContentProperty); }
+            set { SetValue(FiltersContentProperty, value); }
+        }
+        public static DependencyProperty FiltersContentProperty
+        {
+            get;
+        } = DependencyProperty.Register(nameof(FiltersContent), typeof(object), typeof(BoardStandard), new PropertyMetadata(null, null));
 
+
+        public object BoardOptionsContent
+        {
+            get { return GetValue(BoardOptionsContentProperty); }
+            set { SetValue(BoardOptionsContentProperty, value); }
+        }
+        public static DependencyProperty BoardOptionsContentProperty
+        {
+            get;
+        } = DependencyProperty.Register(nameof(BoardOptionsContent), typeof(object), typeof(BoardStandard), new PropertyMetadata(null, null));
 
         public BoardStandard() => this.DefaultStyleKey = typeof(BoardStandard);
     }
