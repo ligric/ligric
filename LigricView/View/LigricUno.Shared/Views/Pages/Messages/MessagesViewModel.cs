1ef9616aa22f64bdc0484f3f3817d65aedbbcd66
limeniye <limeniye@gamanet.com>
2022-05-10 04:22:22 +0300

    - Added test Messages;

--- LigricView/View/LigricUno.Shared/Views/Pages/Messages/MessagesViewModel.cs
+++ LigricView/View/LigricUno.Shared/Views/Pages/Messages/MessagesViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace LigricUno.Views.Pages.Messages
+﻿using System.Collections.ObjectModel;
+
+namespace LigricUno.Views.Pages.Messages
 {
     public class MessagesViewModel
     {
+        public ObservableCollection<MessageItem> Messages { get; } = new ObservableCollection<MessageItem>()
+        {
+            new MessageItem("", "Иван", "Го бухать"),
+            new MessageItem("", "Иван", "Го бухать"),
+            new MessageItem("", "Иван", "Го бухать"),
+            new MessageItem("", "Иван", "Го бухать"),
+            new MessageItem("", "Иван", "Го бухать"),
+            new MessageItem("", "Иван", "Го бухать")
+        };
+    }
+
+    public class MessageItem
+    {
+        public string ImageUri { get; }
+        public string Name { get; }
+        public string LastMessage { get; }
+
+        public MessageItem(string imageUri, string name, string lastMessage)
+        {
+            ImageUri = imageUri;
+            Name = name;
+            LastMessage = lastMessage;
+        }
     }
 }
