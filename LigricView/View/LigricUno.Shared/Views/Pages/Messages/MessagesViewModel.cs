4553d9fa236036a6c5d0888311b73bd22a0c4064
limeniye <limeniye@gamanet.com>
2022-05-10 05:51:19 +0300

    - Hot fixes;

--- LigricView/View/LigricUno.Shared/Views/Pages/Messages/MessagesViewModel.cs
+++ LigricView/View/LigricUno.Shared/Views/Pages/Messages/MessagesViewModel.cs
@@ -7,13 +7,13 @@ namespace LigricUno.Views.Pages.Messages
     {
         public ObservableCollection<MessageItem> Messages { get; } = new ObservableCollection<MessageItem>()
         {
-            new MessageItem("ms-appx:///Assets/Images/limeniye_icon.JPG", "Alexey True", "😁😁😁", DateTime.Now),
+            new MessageItem("ms-appx:///Assets/Images/Alexey True.jpg", "Alexey True", "😁😁😁", DateTime.Now),
             new MessageItem(String.Empty, "Célia B", "I’m just really disappointed that you do not share anything because that’s what friends are supposed to do if you trust each other. But you make me feel like a stranger to you. So let’s keep it this way, I wish you the best for the future, keep the good work and keep it all for you, I won’t support you anymore and you never wanted my support in the first place so it doesn’t matter. Goodbye",
                 DateTime.Now),
             new MessageItem("ms-appx:///Assets/Images/Gogo.jpg", "Gogo 郭", "ty", DateTime.Now),
             new MessageItem("ms-appx:///Assets/Images/elen.jpg", "elen.", "😸", DateTime.Now),
             new MessageItem(String.Empty, "Trip Ray", "🙏🙏🙏", DateTime.Now),
-            new MessageItem("ms-appx:///Assets/Images/Даня Караконстантин.jpg", "Даня Караконстантин", "ыыыы", DateTime.Now)
+            new MessageItem("ms-appx:///Assets/Images/Даня Караконстантин.jpg", "Даня Караконстантин", "ыыыы", DateTime.Now, 3)
         };
     }
 
