f4ea062e6762c0a3deb4a7cb81f1e5aac0c41beb
Mykyta Vtorenko <limeniye@gmail.com>
2022-12-04 00:30:59 +0200

    ViewModel and sign in

---
+++ LigricView/View/LigricUno.Shared/GrpcChannelHalper.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Net.Http;
+
+namespace LigricUno
+{
+    public static class GrpcChannelHalper
+    {
+        public static GrpcChannel GetGrpcChannel()
+        {
+            string address = GetServerAddress();
+
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var httpClientHandler = new HttpClientHandler();
+
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+
+            HttpClient httpClient = null;
+
+            httpClient = new(httpClientHandler);
+
+            return GrpcChannel.ForAddress(address, new GrpcChannelOptions
+            {
+                HttpClient = httpClient,
+                Credentials = ChannelCredentials.SecureSsl
+            });
+        }
+
+        private static string GetServerAddress()
+        {
+            var address = "https://3.72.127.66:5010";
+
+            //---------------------------------------------------------------
+            // TODO : #USE_LOCAL_MODE
+            //---------------------------------------------------------------
+            if (true)
+            {
+#if WINDOWS_UWP
+                address = "https://localhost:5010";
+#endif
+#if __ANDROID__
+                address = "https://10.0.2.2:5010";
+#endif
+            }
+            //---------------------------------------------------------------
+
+            return address;
+        }
+    }
+}
--- LigricView/View/LigricUno.Shared/Views/Pages/Login/LoginViewModel.cs
+++ LigricView/View/LigricUno.Shared/Views/Pages/Login/LoginViewModel.cs
@@ -1,4 +1,8 @@
-﻿using LigricMvvmToolkit.BaseMvvm;
+﻿using Grpc.Net.Client;
+using Ligric.Business;
+using Ligric.Common.Abstractions;
+using Ligric.Common.Types;
+using LigricMvvmToolkit.BaseMvvm;
 using LigricMvvmToolkit.Navigation;
 using LigricMvvmToolkit.RelayCommand;
 using LigricUno.Views.Pages.Board;
@@ -9,20 +13,38 @@ namespace LigricUno.Views.Pages.Login
 {
     public class LoginViewModel : DispatchedBindableBase
     {
+        private readonly IAuthorizationService _authorizationService;
+
         private string _login, _password;
 
         public string Login { get => _login; set => SetProperty(ref _login, value); }
         public string Password { get => _password; set => SetProperty(ref _password, value); }
 
+        public LoginViewModel()
+        {
+            GrpcChannel grpcChannel = GrpcChannelHalper.GetGrpcChannel();
+            var metadataRepos = new MetadataRepository();
+
+            _authorizationService = new AuthorizationService(grpcChannel, metadataRepos);
+            _authorizationService.AuthorizationStateChanged += OnAuthorizationStateChanged;
+        }
+
+        private void OnAuthorizationStateChanged(object sender, Ligric.Common.Types.UserAuthorizationState e)
+        {
+            switch(e)
+            {
+                case UserAuthorizationState.Connected:
+                    Navigation.GoTo(nameof(BoardPage));
+                    break;
+            }
+        }
 
         private RelayCommand _loginCommand;
         public RelayCommand LoginCommand => _loginCommand ?? (_loginCommand = new RelayCommand(LoginMethod));
 
-        private readonly List<(Action<int> Action, int Index)> prerenderPageActions = new List<(Action<int> Action, int Index)>();
-
-        private void LoginMethod(object parameter)
+        private async void LoginMethod(object parameter)
         {
-            Navigation.GoTo(nameof(BoardPage));
+            await _authorizationService.SignInAsync(Login, Password);
         }
     }
 }
