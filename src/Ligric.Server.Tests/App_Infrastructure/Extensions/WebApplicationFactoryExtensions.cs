d1f58f5b6bcf8eaf219b0441080d157cafe121dc
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-10 15:02:29 +0200

    Revert "Not worked tests"
    This reverts commit de0b5e6219939a8c26c16e872a11258b5584cf1a.

---
+++ src/Ligric.Server.Tests/App_Infrastructure/Extensions/WebApplicationFactoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.Testing.Handlers;
+using Ligric.Server.Tests.App_Infrastructure.DelegatingHandlers;
+using System.Net;
+using System.Net.Http;
+
+namespace Ligric.Server.Tests.App_Infrastructure.Extensions
+{
+    public static class WebApplicationFactoryExtensions
+    {
+        public static HttpClient CreateClientForGrpc<TEntryPoint>(this WebApplicationFactory<TEntryPoint> webApplicationFactory) where TEntryPoint : class
+        {
+            return webApplicationFactory.CreateDefaultClient(new DelegatingHandler[]
+            {
+                new CookieContainerHandler(),
+                new OverrideResponseHttpVersionHandler(HttpVersion.Version20)
+            });
+        }
+    }
+}
