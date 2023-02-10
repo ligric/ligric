de0b5e6219939a8c26c16e872a11258b5584cf1a
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-10 15:02:21 +0200

    Not worked tests

---
+++ src/Grpc.Dotnet.Shared.IntegrationTests/GrpcServerApplicationFactoryBase.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace Grpc.Dotnet.Shared.Helpers.IntegrationTests
+{
+    public class GrpcServerApplicationFactoryBase<TStartup, TContext> : WebApplicationFactory<TStartup>
+        where TStartup : class
+        where TContext : DbContext
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.integration.json");
+            builder.UseEnvironment(Environments.Development);
+            builder.ConfigureAppConfiguration((context, conf) =>
+            {
+                conf.AddJsonFile(configPath, optional: true);
+                conf.AddEnvironmentVariables();
+                this.ConfigureAppConfiguration(context, conf);
+            });
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddAuthentication("Test")
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>("Test", options => { });
+
+                var tContextServiceDescriptor = services.FirstOrDefault(s => s.ServiceType == typeof(TContext));
+#pragma warning disable CS8604 // Possible null reference argument.
+				services.Remove(tContextServiceDescriptor);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+				var dbContextOptionsDescriptor = services.FirstOrDefault(x => x.ServiceType == typeof(DbContextOptions<TContext>));
+#pragma warning disable CS8604 // Possible null reference argument.
+				services.Remove(dbContextOptionsDescriptor);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+				services.Replace(ServiceDescriptor.Singleton<TContext, TContext>());
+
+                this.ConfigureTestServices(builder, services);
+            });
+        }
+
+        protected virtual void ConfigureTestServices(IWebHostBuilder builder, IServiceCollection servicesConfiguration)
+        {
+        }
+
+        protected virtual void ConfigureAppConfiguration(WebHostBuilderContext context, IConfigurationBuilder configuration)
+        {
+        }
+    }
+}
---
+++ src/Grpc.Dotnet.Shared.IntegrationTests/WebApplicationFactoryBase.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Grpc.Dotnet.Shared.Helpers.Rpc.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace Grpc.Dotnet.Shared.Helpers.IntegrationTests
+{
+    public class WebApplicationFactoryBase<TStartup, TContext> : WebApplicationFactory<TStartup>
+        where TStartup : class
+        where TContext : class
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            var builder = base.CreateHostBuilder();
+            return builder;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.integration.json");
+
+            builder.ConfigureAppConfiguration((context, conf) =>
+            {
+                conf.AddJsonFile(configPath, optional: true);
+                conf.AddEnvironmentVariables();
+            });
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddAuthentication("Test")
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>("Test", options => { });
+
+                services.Replace(ServiceDescriptor.Singleton<TContext, TContext>());
+
+                services.AddSingleton(typeof(IServiceClient<>), typeof(ServiceClientMock<>));
+            });
+        }
+    }
+}
