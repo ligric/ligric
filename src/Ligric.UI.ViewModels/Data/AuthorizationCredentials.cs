02b3232a9670c2ce20ddf66d65161d0a5b538f33
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-13 15:57:35 +0200

    SetAuthorizationMode binding

---
+++ src/Ligric.UI.ViewModels/Data/AuthorizationCredentials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ligric.UI.ViewModels.Data
+{
+	public enum AutorizationMode
+	{
+		SignIn,
+		SignUp
+	}
+
+	public record AuthorizationCredentials
+	{
+		public AutorizationMode AutorizationMode { get; init; }
+
+		public string? UserName { get; init; }
+
+		public string? Password { get; init; }
+
+		public string? RepeatedPassword { get; init; }
+	}
+}
