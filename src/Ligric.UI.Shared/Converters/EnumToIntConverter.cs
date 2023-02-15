cf144a830b252aa36ddf92900cd9be1c36074407
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-15 14:37:58 +0200

    AuthorizationMode binding doesnt work

---
+++ src/Ligric.UI.Shared/Converters/EnumToIntConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Ligric.UI.Converters
+{
+	public class EnumToIntConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return (int)value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			string? valueString = value.ToString();
+			if (valueString == null)
+			{
+				return DependencyProperty.UnsetValue;
+			}
+
+			return Enum.Parse(targetType, valueString);
+		}
+	}
+}
