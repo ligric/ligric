05ef8c0eebc8e0a4d9de8a98ea9dc955a7349be7
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-16 12:15:27 +0200

    [UserAPIs] added uses api permissions

---
+++ src/Ligric.Server.Grpc/Database/InitializeValues.sql
@@ -0,0 +1,5 @@
+INSERT INTO [Permissions](Id, Name) VALUES (1, 'Read');
+INSERT INTO [Permissions](Id, Name) VALUES (2, 'Write');
+INSERT INTO [Permissions](Id, Name) VALUES (4, 'Update');
+INSERT INTO [Permissions](Id, Name) VALUES (8, 'Remove');
+INSERT INTO [Permissions](Id, Name) VALUES (16, 'Share');
