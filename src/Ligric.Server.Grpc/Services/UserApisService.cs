3d000d93c8ffee1629c41cb50681ead35c585eb6
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-16 22:52:00 +0200

    Created UserApisService

---
+++ src/Ligric.Server.Grpc/Services/UserApisService.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Ligric.Protos;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Ligric.Server.Grpc.Services
+{
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class UserApisService : UserApies.UserApiesBase
+	{
+		private readonly IMediator _mediator;
+
+		public UserApisService(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[Authorize]
+		public override async Task<SaveApiResponse> Save(SaveApiRequest saveApi, ServerCallContext context)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Authorize]
+		public override async Task ApiesSubscribe(Empty empty, IServerStreamWriter<ApiesChanged> responseStream, ServerCallContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
