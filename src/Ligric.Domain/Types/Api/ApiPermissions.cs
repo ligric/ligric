315db7b455069dcfe937316859b581e53bb8b55e
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-16 13:07:06 +0200

    ApiPermissions

---
+++ src/Ligric.Domain/Types/Api/ApiPermissions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ligric.Domain.Types.Api
+{
+	[Flags]
+	public enum ApiPermissions
+	{
+		None = 0,
+		Read = 1,
+		Activity = 2,
+		Update = 4,
+		Remove = 8,
+		Share = 16,
+	}
+}
