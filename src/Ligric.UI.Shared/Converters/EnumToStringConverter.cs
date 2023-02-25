9eb2797eb9017ecdc4f50ff91649f3aa21bbec56
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-25 19:41:05 +0200

    EnumToInt changed to EnumToString

---
+++ src/Ligric.UI.Shared/Converters/EnumToStringConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace Ligric.UI.Converters
+{
+	public class EnumToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return value?.ToString() ?? DependencyProperty.UnsetValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
