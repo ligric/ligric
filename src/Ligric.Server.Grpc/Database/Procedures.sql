976255934f1f28c06c5c1af0a54ef5dfe082e9fa
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-26 18:53:56 +0200

    Created [GetAllUserIdsThatDontHaveTheseAPI] procedure

--- src/Ligric.Server.Grpc/Database/Procedures.sql
+++ src/Ligric.Server.Grpc/Database/Procedures.sql
@@ -11,6 +11,24 @@ CREATE PROCEDURE [GetAllowedAPI] @userId INT
 	END
 GO
 
+DROP PROCEDURE IF EXISTS [GetUserIdsThatDontHaveTheseApi]
+GO
+
+CREATE PROCEDURE [GetUserIdsThatDontHaveTheseApi] @userApiId INT
+AS
+BEGIN
+	DECLARE @apiId AS INT;
+	SET @apiId = (select TOP 1 _userApi.[ApiId] from [UserAPIs] _userApi where _userApi.[Id] = @userApiId)
+
+	SELECT _user.[Id]
+	FROM [Users] _user
+	WHERE NOT EXISTS (
+		SELECT *
+		FROM [UserAPIs] _userApi
+		WHERE _userApi.[ApiId] = @apiId AND _userApi.[UserId] = _user.[Id])
+END
+GO
+
 -- This is just procedures example:
 
 
