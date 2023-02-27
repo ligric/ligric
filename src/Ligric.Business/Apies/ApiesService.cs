43c325b85756beddf037c018858f2fe57c15b96a
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-27 20:25:57 +0200

    Added Api share permissions for client side

--- src/Ligric.Business/Apies/ApiesService.cs
+++ src/Ligric.Business/Apies/ApiesService.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Utils;
+using Utils.Extensions;
 using static Ligric.Protos.UserApis;
 
 namespace Ligric.Business.Apies
