45e9a6018994a0baba75b4c12178dd44d56a36d8
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-28 17:49:42 +0200

    ApiExtensions

---
+++ src/Ligric.Server.Domain/TypeExtensions/ApiExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Ligric.Domain.Types.Api;
+using Ligric.Server.Domain.Entities.Apies;
+
+namespace Ligric.Server.Domain.TypeExtensions
+{
+	public static class ApiExtensions
+	{
+		public static ApiDto ToApiDto(this ApiEntity api)
+		{
+			return new ApiDto(
+				api.Id,
+				api.PublicKey ?? throw new System.ArgumentException($"Api [{api.Id}] public key is null"),
+				api.PrivateKey ?? throw new System.ArgumentException($"Api [{api.Id}] private key is null"));
+		}
+
+	}
+}
