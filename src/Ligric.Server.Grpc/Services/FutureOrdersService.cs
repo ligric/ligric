fe97c3cc332986f2094a1838f710a33fbd962860
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-28 12:57:12 +0200

    Added FutureOrdersService for ServerSide

---
+++ src/Ligric.Server.Grpc/Services/FutureOrdersService.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using Ligric.Protos;
+using Ligric.Server.Grpc.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Ligric.Server.Grpc.Services
+{
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class FutureOrdersService : FutureOrders.FutureOrdersBase
+	{
+		private readonly IMediator _mediator;
+
+		public FutureOrdersService(
+			IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[Authorize]
+		public override async Task<ResponseResult> AddObserverUserApi(AddObserverUserApiRequest request, ServerCallContext context)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Authorize]
+		public override async Task OrdersSubscribe(OrdersSubscribeRequest request, IServerStreamWriter<OrdersChanged> responseStream, ServerCallContext context)
+		{
+			throw new NotImplementedException();
+			//await _userApiObserver.GetApisAsObservable(request.UserId)
+			//	.ToAsyncEnumerable()
+			//	.ForEachAwaitAsync(async (x) =>
+			//	{
+			//		if (x.UserId == request.UserId)
+			//		{
+			//			await responseStream.WriteAsync(new ApisChanged
+			//			{
+			//				Action = x.Action.ToProtosAction(),
+			//				Api = new ApiClient
+			//				{
+			//					Id = x.Api.UserApiId ?? throw new ArgumentNullException("ApisSubscribe UserApiId is null"),
+			//					Name = x.Api.Name,
+			//					Permissions = x.Api.Permissions
+			//				}
+			//			});
+			//		}
+			//	}, context.CancellationToken)
+			//	.ConfigureAwait(false);
+		}
+	}
+}
