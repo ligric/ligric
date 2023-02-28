320b5e8e8f2c1db21b467b280252902d56d9c03b
Mykyta Vtorenko <limeniye@gmail.com>
2023-02-28 12:25:24 +0200

    Added FuturePositions

---
+++ src/Ligric.UI.ViewModels/Presentation/FuturePositionsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using Ligric.UI.ViewModels.Data;
+
+namespace Ligric.UI.ViewModels.Presentation
+{
+	public class FuturePositionsViewModel
+	{
+		public ObservableCollection<PositionViewModel> Positions { get; } = new ObservableCollection<PositionViewModel>();
+
+		//private async void OnPositionChanged(object sender, (BinanceFuturesPositionDto Position, ActionCollectionEnum Action) e)
+		//{
+		////    var entity = CurrentEntities.FirstOrDefault(x => string.Equals(x.Symbol, e.Position.Symbol));
+
+		////    if (e.Action == ActionCollectionEnum.Added)
+		////    {
+		////        await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+		////        {
+		////            entity.Positions.AddAndRiseEvent(new PositionViewModel
+		////            {
+		////                Symbol = e.Position.Symbol,
+		////                CurrentPrice = e.Position.CurrentPrice,
+		////                OpenPrice = e.Position.OpenPrice,
+		////                PnL = e.Position.PnL,
+		////                PnLPercent = e.Position.PnLPercent,
+		////                Quantity = e.Position.Quantity,
+		////                Side = e.Position.Side
+		////            });
+		////        });
+		////    }
+		////    else
+		////    {
+		////        var removePosition = entity.Positions.FirstOrDefault(x => string.Equals(x.Symbol, e.Position.Symbol) && string.Equals(x.Side, e.Position.Side));
+		////        if (removePosition != null)
+		////        {
+		////            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+		////            {
+		////                entity.Positions.Remove(removePosition);
+		////            });
+		////        }
+		////    }
+		//}
+
+	}
+}
--- src/Ligric.UI.ViewModels/Presentation/FuturesViewModel.cs
+++ src/Ligric.UI.ViewModels/Presentation/FuturesViewModel.cs
@@ -11,48 +11,16 @@ namespace Ligric.UI.ViewModels.Presentation
 		{
 			Api = new ApisViewModel(apiesService, orderService);
 			FuturesOrderPositions = new FutureOrdersViewModel(orderService);
+			FuturePositions = new FuturePositionsViewModel();
 		}
 
 		public ApisViewModel Api { get; }
 
 		public FutureOrdersViewModel FuturesOrderPositions { get; }
 
+		public FuturePositionsViewModel FuturePositions { get; }
 
-		//public ObservableCollection<PositionViewModel> Positions { get; } = new ObservableCollection<PositionViewModel>();
-
-		//private async void OnPositionChanged(object sender, (BinanceFuturesPositionDto Position, ActionCollectionEnum Action) e)
-		//{
-		////    var entity = CurrentEntities.FirstOrDefault(x => string.Equals(x.Symbol, e.Position.Symbol));
-
-		////    if (e.Action == ActionCollectionEnum.Added)
-		////    {
-		////        await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-		////        {
-		////            entity.Positions.AddAndRiseEvent(new PositionViewModel
-		////            {
-		////                Symbol = e.Position.Symbol,
-		////                CurrentPrice = e.Position.CurrentPrice,
-		////                OpenPrice = e.Position.OpenPrice,
-		////                PnL = e.Position.PnL,
-		////                PnLPercent = e.Position.PnLPercent,
-		////                Quantity = e.Position.Quantity,
-		////                Side = e.Position.Side
-		////            });
-		////        });
-		////    }
-		////    else
-		////    {
-		////        var removePosition = entity.Positions.FirstOrDefault(x => string.Equals(x.Symbol, e.Position.Symbol) && string.Equals(x.Side, e.Position.Side));
-		////        if (removePosition != null)
-		////        {
-		////            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-		////            {
-		////                entity.Positions.Remove(removePosition);
-		////            });
-		////        }
-		////    }
-		//}
-
+		
 		//private void OnPriceChanged(object sender, (string Symbol, decimal Price) e)
 		//{
 		//    //var entity = CurrentEntities.FirstOrDefault(x => string.Equals(x.Symbol, e.Symbol));
