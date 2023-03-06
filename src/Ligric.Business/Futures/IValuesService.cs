d7004d9a02ab41f3291889fb06b7a2984064f71f
Mykyta Vtorenko <limeniye@gmail.com>
2023-03-06 11:52:35 +0200

    IPricesService -> IValuesService

---
+++ src/Ligric.Business/Futures/IValuesService.cs
@@ -0,0 +1,14 @@
+﻿using Ligric.Domain.Types.Future;
+using System;
+using System.Collections.Generic;
+using Utils;
+
+namespace Ligric.Business.Futures
+{
+	public interface IValuesService : IDisposable
+	{
+		IReadOnlyDictionary<string, decimal> Values { get; }
+
+		event EventHandler<NotifyDictionaryChangedEventArgs<string, decimal>> ValuesChanged;
+	}
+}
