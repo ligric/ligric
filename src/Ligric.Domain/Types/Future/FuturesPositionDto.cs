fa0f248837d2768ee2dda0c372ac02b13a6290a4
Mykyta Vtorenko <limeniye@gmail.com>
2023-03-06 20:09:51 +0200

    FuturesPositionDto changed: removed unecessury properties

--- src/Ligric.Domain/Types/Future/FuturesPositionDto.cs
+++ src/Ligric.Domain/Types/Future/FuturesPositionDto.cs
@@ -2,32 +2,32 @@
 {
 	public record FuturesPositionDto
 	{
-		public long OrderId { get; init; }
+		public long Id { get; init; }
 
 		public string Symbol { get; init; }
 
 		public Side Side { get; init; }
 
-		public decimal Quantity { get; init; }
+		//public decimal Quantity { get; init; }
 
-		public decimal OpenPrice { get; init; }
+		public decimal EntryPrice { get; init; }
 
-		public decimal CurrentPrice { get; init; }
+		//public decimal CurrentPrice { get; init; }
 
-		public decimal GrossPNL { get; init; }
+		//public decimal GrossPNL { get; init; }
 
-		public decimal GrossROE { get; init; }
+		//public decimal GrossROE { get; init; }
 
-		public FuturesPositionDto(long orderId, string symbol, Side side, decimal quantity, decimal openPrice, decimal currentPrice, decimal grossPNL, decimal grossROE)
+		public FuturesPositionDto(long orderId, string symbol, Side side, /*decimal quantity, */decimal openPrice/*, decimal currentPrice, decimal grossPNL, decimal grossROE*/)
 		{
-			OrderId = orderId;
+			Id = orderId;
 			Symbol = symbol;
 			Side = side;
-			Quantity = quantity;
-			OpenPrice = openPrice;
-			CurrentPrice = currentPrice;
-			GrossPNL = grossPNL;
-			GrossROE = grossROE;
+			//Quantity = quantity;
+			EntryPrice = openPrice;
+			//CurrentPrice = currentPrice;
+			//GrossPNL = grossPNL;
+			//GrossROE = grossROE;
 		}
 	}
 }
