e1d43cce233927bac41fc54a915e808cbd4ec260
Mykyta Vtorenko <limeniye@gmail.com>
2023-03-06 23:36:08 +0200

    Business added Positions

--- src/Ligric.Business/Extensions/TypeExtensions.cs
+++ src/Ligric.Business/Extensions/TypeExtensions.cs
@@ -15,11 +15,18 @@ namespace Ligric.Business.Extensions
 		public static FuturesOrderDto ToFuturesOrderDto(this FuturesOrder futureOrder)
 		{
 			 return new FuturesOrderDto(
-				 futureOrder.Id, futureOrder.Symbol, futureOrder.Side.ToSideDto(),
+				 futureOrder.Id, futureOrder.Symbol, futureOrder.Side.ToOrderSideDto(),
 				 decimal.Parse(futureOrder.Quantity), decimal.Parse(futureOrder.Price), decimal.Parse(futureOrder.Value));
 		}
 
-		public static Domain.Types.OrderSide ToSideDto(this OrderSide sideInput)
+		public static FuturesPositionDto ToFuturesPositionDto(this FuturesPosition futuresPosition)
+		{
+			 return new FuturesPositionDto(
+				 futuresPosition.Id, futuresPosition.Symbol, futuresPosition.Side.ToPositionSideDto(),
+				 decimal.Parse(futuresPosition.EntryPrice));
+		}
+
+		public static Domain.Types.OrderSide ToOrderSideDto(this OrderSide sideInput)
 		{
 			switch (sideInput)
 			{
@@ -27,6 +34,20 @@ namespace Ligric.Business.Extensions
 					return Domain.Types.OrderSide.Sell;
 				case OrderSide.Buy:
 					return Domain.Types.OrderSide.Buy;
+			}
+			throw new NotImplementedException();
+		}
+
+		public static Domain.Types.PositionSide ToPositionSideDto(this PositionSide sideInput)
+		{
+			switch (sideInput)
+			{
+				case PositionSide.Short:
+					return Domain.Types.PositionSide.Short;
+				case PositionSide.Long:
+					return Domain.Types.PositionSide.Long;
+				case PositionSide.Both:
+					return Domain.Types.PositionSide.Both;
 			}
 			throw new NotImplementedException();
 		}
