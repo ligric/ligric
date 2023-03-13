cd6ae17cf0463c86d510cc6e5a1da0a03bfbfd57
Mykyta Vtorenko <limeniye@gmail.com>
2023-03-13 14:19:34 +0200

    Auth service response extensions

---
+++ src/Ligric.Service.AuthService/Ligric.Service.AuthService.Api/Extensions/ResponseHelper.cs
@@ -0,0 +1,18 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Ligric.Service.AuthService.Api;
+
+namespace Ligric.Service
+{
+	public static class ResponseHelper
+    {
+        public static ResponseResult GetSuccessResponseResult()
+        {
+            return new ResponseResult { IsSuccess = true, OccurredOn = Timestamp.FromDateTime(DateTime.UtcNow) };
+        }
+
+		public static ResponseResult GetFailedResponseResult()
+		{
+			return new ResponseResult { IsSuccess = false, OccurredOn = Timestamp.FromDateTime(DateTime.UtcNow) };
+		}
+	}
+}
