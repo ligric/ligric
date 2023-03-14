98c2c05d7bc796108ebf5778f348eca1d4fc10f9
Mykyta Vtorenko <limeniye@gmail.com>
2023-03-14 19:35:23 +0200

    Swagger

--- src/Ligric.Service.Gateway/Program.cs
+++ src/Ligric.Service.Gateway/Program.cs
@@ -1,9 +1,42 @@
+using Ligric.Rpc.Contracts;
+using Ligric.Service.Gateway.ExceptionHandler;
+
+const string CORS_POLICY = "_corsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddGrpc();
+//builder.Services.AddGrpc();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: CORS_POLICY,
+		policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+});
+
+builder.Services.AddControllers(options => { options.Filters.Add(new HttpResponseExceptionActionFilter()); });
+
+builder.Services.AddGrpcClient<Auth.AuthClient>((o) =>
+{
+	o.Address = new Uri(builder.Configuration["ClientUrls:AuthClient"] ?? throw new ArgumentNullException("AuthClient not found inside configuration."));
+});
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.Run();
