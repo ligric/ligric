571f0ee0ca43cc91123ea1ee8fcde6aa72b044ee
Mykyta Vtorenko <limeniye@gmail.com>
2023-03-15 11:08:04 +0200

    Nhibernate. Mapping

---
+++ src/Ligric.Service.AuthService/Ligric.Service.AuthService.Domain/Exceptions/MappingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+using Ligric.Service.AuthService.Domain.Entities;
+
+namespace Ligric.Service.AuthService.Domain.Exceptions
+{
+	public static class MappingExtensions
+	{
+		public static void MapBase<T>(this ClassMap<T> mapClass) where T : EntityBase
+		{
+			mapClass.Id(x => x.Id).GeneratedBy.Identity();
+
+			mapClass.Map(x => x.Deleted);
+			mapClass.Map(x => x.UpdateDate);
+			mapClass.Map(x => x.CreateDate).Generated.Insert();
+		}
+	}
+}
---
+++ src/Ligric.Service.AuthService/Ligric.Service.AuthService.Domain/Mapping/UserMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using Ligric.Service.AuthService.Domain.Entities;
+using Ligric.Service.AuthService.Domain.Exceptions;
+
+namespace Ligric.Domain.Mapping
+{
+    public class UserMap : ClassMap<UserEntity> 
+    {
+        public UserMap()
+        {
+            Table("[Users]");
+            this.MapBase();
+
+            Map(x => x.UserName);
+            Map(x => x.Password);
+            Map(x => x.Salt);
+        }
+    }
+}
