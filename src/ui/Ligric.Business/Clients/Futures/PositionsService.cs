7f94bcf9d1b9c1260a52c94529eaac39b373a9f6
Mykyta Vtorenko <limeniye@gmail.com>
2023-04-17 01:14:44 +0300

    Futures commented

--- src/ui/Ligric.Business/Clients/Futures/PositionsService.cs
+++ src/ui/Ligric.Business/Clients/Futures/PositionsService.cs
@@ -1,92 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Ligric.Core.Types.Future;
-using Utils;
-using Ligric.Business.Authorization;
-using System.Threading;
-using System.Linq;
-using Ligric.Business.Metadata;
-using Ligric.Business.Extensions;
-using Ligric.Business.Futures;
-using static Ligric.Protobuf.Futures;
-using Ligric.Protobuf;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.Threading.Tasks;
+//using Ligric.Core.Types.Future;
+//using Utils;
+//using Ligric.Business.Authorization;
+//using System.Threading;
+//using System.Linq;
+//using Ligric.Business.Metadata;
+//using Ligric.Business.Extensions;
+//using Ligric.Business.Futures;
+//using static Ligric.Protobuf.Futures;
+//using Ligric.Protobuf;
 
-namespace Ligric.Business.Clients.Futures
-{
-	public class PositionsService : IPositionsService
-	{
-		private int syncOrderChanged = 0;
-		private readonly Dictionary<long, FuturesPositionDto> _positions = new Dictionary<long, FuturesPositionDto>();
-		private CancellationTokenSource? _futuresSubscribeCalcellationToken;
-		private readonly IAuthorizationService _authorizationService;
-		private readonly IMetadataManager _metadataManager;
-		private readonly FuturesClient _futuresClient;
+//namespace Ligric.Business.Clients.Futures
+//{
+//	public class PositionsService : IPositionsService
+//	{
+//		private int syncOrderChanged = 0;
+//		private readonly Dictionary<long, FuturesPositionDto> _positions = new Dictionary<long, FuturesPositionDto>();
+//		private CancellationTokenSource? _futuresSubscribeCalcellationToken;
+//		private readonly IAuthorizationService _authorizationService;
+//		private readonly IMetadataManager _metadataManager;
+//		private readonly FuturesClient _futuresClient;
 
-		internal PositionsService(
-			FuturesClient futuresClient,
-			IMetadataManager metadataRepos,
-			IAuthorizationService authorizationService)
-		{
-			_metadataManager = metadataRepos;
-			_authorizationService = authorizationService;
-			_futuresClient = futuresClient;
-		}
+//		internal PositionsService(
+//			FuturesClient futuresClient,
+//			IMetadataManager metadataRepos,
+//			IAuthorizationService authorizationService)
+//		{
+//			_metadataManager = metadataRepos;
+//			_authorizationService = authorizationService;
+//			_futuresClient = futuresClient;
+//		}
 
-		public IReadOnlyDictionary<long, FuturesPositionDto> Positions => new ReadOnlyDictionary<long, FuturesPositionDto>(_positions);
+//		public IReadOnlyDictionary<long, FuturesPositionDto> Positions => new ReadOnlyDictionary<long, FuturesPositionDto>(_positions);
 
-		public event EventHandler<NotifyDictionaryChangedEventArgs<long, FuturesPositionDto>>? PositionsChanged;
+//		public event EventHandler<NotifyDictionaryChangedEventArgs<long, FuturesPositionDto>>? PositionsChanged;
 
-		public Task AttachStreamAsync(long userApiId)
-		{
-			if (_futuresSubscribeCalcellationToken != null
-				&& !_futuresSubscribeCalcellationToken.IsCancellationRequested)
-			{
-				return Task.CompletedTask;
-			}
+//		public Task AttachStreamAsync(long userApiId)
+//		{
+//			if (_futuresSubscribeCalcellationToken != null
+//				&& !_futuresSubscribeCalcellationToken.IsCancellationRequested)
+//			{
+//				return Task.CompletedTask;
+//			}
 
-			var userId = _authorizationService.CurrentUser.Id ?? throw new NullReferenceException("[AttachStreamAsync] UserId is null");
+//			var userId = _authorizationService.CurrentUser.Id ?? throw new NullReferenceException("[AttachStreamAsync] UserId is null");
 
-			_futuresSubscribeCalcellationToken = new CancellationTokenSource();
-			return StreamApiSubscribeCall(userId, userApiId, _futuresSubscribeCalcellationToken.Token);
-		}
+//			_futuresSubscribeCalcellationToken = new CancellationTokenSource();
+//			return StreamApiSubscribeCall(userId, userApiId, _futuresSubscribeCalcellationToken.Token);
+//		}
 
-		public void DetachStream() => throw new NotImplementedException();
-		public void Dispose()
-		{
+//		public void DetachStream() => throw new NotImplementedException();
+//		public void Dispose()
+//		{
 
-		}
+//		}
 
-		private Task StreamApiSubscribeCall(long userId, long userApiId, CancellationToken token)
-		{
-			var call = _futuresClient.PositionsSubscribe(
-				request: new FuturesSubscribeRequest { UserId = userId, UserApiId = userApiId },
-				headers: _metadataManager.CurrentMetadata,
-				cancellationToken: token);
+//		private Task StreamApiSubscribeCall(long userId, long userApiId, CancellationToken token)
+//		{
+//			var call = _futuresClient.PositionsSubscribe(
+//				request: new FuturesSubscribeRequest { UserId = userId, UserApiId = userApiId },
+//				headers: _metadataManager.CurrentMetadata,
+//				cancellationToken: token);
 
-			return call.ResponseStream
-				.ToAsyncEnumerable()
-				.Finally(() => call.Dispose())
-				.ForEachAsync((api) =>
-				{
-					OnFuturesChanged(api);
-				}, token);
-		}
+//			return call.ResponseStream
+//				.ToAsyncEnumerable()
+//				.Finally(() => call.Dispose())
+//				.ForEachAsync((api) =>
+//				{
+//					OnFuturesChanged(api);
+//				}, token);
+//		}
 
-		private void OnFuturesChanged(PositionsChanged positionsChanged)
-		{
-			switch (positionsChanged.Action)
-			{
-				case Rpc.Contracts.Action.Added:
-					var positionDto = positionsChanged.Position.ToFuturesPositionDto();
-					_positions.SetAndRiseEvent(this, PositionsChanged, positionsChanged.Position.Id, positionDto, ref syncOrderChanged);
-					break;
-				case Rpc.Contracts.Action.Removed:
-					_positions.RemoveAndRiseEvent(this, PositionsChanged, positionsChanged.Position.Id, ref syncOrderChanged);
-					break;
-				case Rpc.Contracts.Action.Changed: goto case Rpc.Contracts.Action.Added;
-			}
-		}
-	}
-}
+//		private void OnFuturesChanged(PositionsChanged positionsChanged)
+//		{
+//			switch (positionsChanged.Action)
+//			{
+//				case Rpc.Contracts.Action.Added:
+//					var positionDto = positionsChanged.Position.ToFuturesPositionDto();
+//					_positions.SetAndRiseEvent(this, PositionsChanged, positionsChanged.Position.Id, positionDto, ref syncOrderChanged);
+//					break;
+//				case Rpc.Contracts.Action.Removed:
+//					_positions.RemoveAndRiseEvent(this, PositionsChanged, positionsChanged.Position.Id, ref syncOrderChanged);
+//					break;
+//				case Rpc.Contracts.Action.Changed: goto case Rpc.Contracts.Action.Added;
+//			}
+//		}
+//	}
+//}
--- src/ui/Ligric.Business/Clients/Futures/ValuesService.cs
+++ src/ui/Ligric.Business/Clients/Futures/ValuesService.cs
@@ -1,94 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Grpc.Net.Client;
-using static Ligric.Protobuf.Futures;
-using Utils;
-using Ligric.Business.Authorization;
-using System.Threading;
-using Ligric.Protobuf;
-using System.Linq;
-using Ligric.Business.Metadata;
-using Ligric.Business.Extensions;
-using Ligric.Business.Futures;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.Threading.Tasks;
+//using Grpc.Net.Client;
+//using static Ligric.Protobuf.Futures;
+//using Utils;
+//using Ligric.Business.Authorization;
+//using System.Threading;
+//using Ligric.Protobuf;
+//using System.Linq;
+//using Ligric.Business.Metadata;
+//using Ligric.Business.Extensions;
+//using Ligric.Business.Futures;
 
-namespace Ligric.Business.Clients.Futures
-{
-	public class ValuesService : IValuesService
-	{
-		private int syncValuesChanged = 0;
-		private readonly Dictionary<string, decimal> _values = new Dictionary<string, decimal>();
-		private CancellationTokenSource? _valuesSubscribeCalcellationToken;
-		private readonly IAuthorizationService _authorizationService;
-		private readonly IMetadataManager _metadataManager;
-		private readonly FuturesClient _futuresClient;
+//namespace Ligric.Business.Clients.Futures
+//{
+//	public class ValuesService : IValuesService
+//	{
+//		private int syncValuesChanged = 0;
+//		private readonly Dictionary<string, decimal> _values = new Dictionary<string, decimal>();
+//		private CancellationTokenSource? _valuesSubscribeCalcellationToken;
+//		private readonly IAuthorizationService _authorizationService;
+//		private readonly IMetadataManager _metadataManager;
+//		private readonly FuturesClient _futuresClient;
 
-		internal ValuesService(
-			FuturesClient futuresClient,
-			IMetadataManager metadataRepos,
-			IAuthorizationService authorizationService)
-		{
-			_metadataManager = metadataRepos;
-			_authorizationService = authorizationService;
-			_futuresClient = futuresClient;
-		}
+//		internal ValuesService(
+//			FuturesClient futuresClient,
+//			IMetadataManager metadataRepos,
+//			IAuthorizationService authorizationService)
+//		{
+//			_metadataManager = metadataRepos;
+//			_authorizationService = authorizationService;
+//			_futuresClient = futuresClient;
+//		}
 
-		public IReadOnlyDictionary<string, decimal> Values => new ReadOnlyDictionary<string, decimal>(_values);
+//		public IReadOnlyDictionary<string, decimal> Values => new ReadOnlyDictionary<string, decimal>(_values);
 
-		public event EventHandler<NotifyDictionaryChangedEventArgs<string, decimal>>? ValuesChanged;
+//		public event EventHandler<NotifyDictionaryChangedEventArgs<string, decimal>>? ValuesChanged;
 
-		public Task AttachStreamAsync(long userApiId)
-		{
-			if (_valuesSubscribeCalcellationToken != null
-				&& !_valuesSubscribeCalcellationToken.IsCancellationRequested)
-			{
-				return Task.CompletedTask;
-			}
+//		public Task AttachStreamAsync(long userApiId)
+//		{
+//			if (_valuesSubscribeCalcellationToken != null
+//				&& !_valuesSubscribeCalcellationToken.IsCancellationRequested)
+//			{
+//				return Task.CompletedTask;
+//			}
 
-			var userId = _authorizationService.CurrentUser.Id ?? throw new NullReferenceException("[AttachStreamAsync] UserId is null");
+//			var userId = _authorizationService.CurrentUser.Id ?? throw new NullReferenceException("[AttachStreamAsync] UserId is null");
 
-			_valuesSubscribeCalcellationToken = new CancellationTokenSource();
-			return StreamValuesSubscribeCall(userId, userApiId, _valuesSubscribeCalcellationToken.Token);
-		}
+//			_valuesSubscribeCalcellationToken = new CancellationTokenSource();
+//			return StreamValuesSubscribeCall(userId, userApiId, _valuesSubscribeCalcellationToken.Token);
+//		}
 
-		public void DetachStream() => throw new NotImplementedException();
+//		public void DetachStream() => throw new NotImplementedException();
 
-		public void Dispose()
-		{
+//		public void Dispose()
+//		{
 
-		}
+//		}
 
-		private Task StreamValuesSubscribeCall(long userId, long userApiId, CancellationToken token)
-		{
-			var call = _futuresClient.ValuesSubscribe(
-				request: new FuturesSubscribeRequest { UserId = userId, UserApiId = userApiId },
-				headers: _metadataManager.CurrentMetadata,
-				cancellationToken: token);
+//		private Task StreamValuesSubscribeCall(long userId, long userApiId, CancellationToken token)
+//		{
+//			var call = _futuresClient.ValuesSubscribe(
+//				request: new FuturesSubscribeRequest { UserId = userId, UserApiId = userApiId },
+//				headers: _metadataManager.CurrentMetadata,
+//				cancellationToken: token);
 
-			return call.ResponseStream
-				.ToAsyncEnumerable()
-				.Finally(() => call.Dispose())
-				.ForEachAsync((api) =>
-				{
-					OnFuturesChanged(api);
-				}, token);
-		}
+//			return call.ResponseStream
+//				.ToAsyncEnumerable()
+//				.Finally(() => call.Dispose())
+//				.ForEachAsync((api) =>
+//				{
+//					OnFuturesChanged(api);
+//				}, token);
+//		}
 
-		private void OnFuturesChanged(ValuesChanged valuesChanged)
-		{
-			var symbol = valuesChanged.Value.Symbol;
-			var value = decimal.Parse(valuesChanged.Value.Value);
-			switch (valuesChanged.Action)
-			{
-				case Rpc.Contracts.Action.Added:
-					_values.SetAndRiseEvent(this, ValuesChanged, symbol, value, ref syncValuesChanged);
-					break;
-				case Rpc.Contracts.Action.Removed:
-					_values.RemoveAndRiseEvent(this, ValuesChanged, symbol, ref syncValuesChanged);
-					break;
-				case Rpc.Contracts.Action.Changed: goto case Rpc.Contracts.Action.Added;
-			}
-		}
-	}
-}
+//		private void OnFuturesChanged(ValuesChanged valuesChanged)
+//		{
+//			var symbol = valuesChanged.Value.Symbol;
+//			var value = decimal.Parse(valuesChanged.Value.Value);
+//			switch (valuesChanged.Action)
+//			{
+//				case Rpc.Contracts.Action.Added:
+//					_values.SetAndRiseEvent(this, ValuesChanged, symbol, value, ref syncValuesChanged);
+//					break;
+//				case Rpc.Contracts.Action.Removed:
+//					_values.RemoveAndRiseEvent(this, ValuesChanged, symbol, ref syncValuesChanged);
+//					break;
+//				case Rpc.Contracts.Action.Changed: goto case Rpc.Contracts.Action.Added;
+//			}
+//		}
+//	}
+//}
