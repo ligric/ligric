91deb1a9746e7828dd5ee3fb6239fc5756db6304
Mykyta Vtorenko <limeniye@gmail.com>
2023-04-18 05:09:45 +0300

    CryptoOBserver simple realization

---
+++ src/services/Ligric.Service.FuturesService/Ligric.Service.FuturesService.Api/Helpers/ActionHelper.cs
@@ -0,0 +1,50 @@
+﻿using Utils;
+using Action = Ligric.Protobuf.Action;
+
+namespace Ligric.Service.FuturesService.Api.Helpers
+{
+    public static class ActionHelper
+    {
+        public static Action ToProtosAction(this EventAction action)
+        {
+            switch (action)
+            {
+                case EventAction.Added:
+                    return Action.Added;
+                case EventAction.Removed:
+                    return Action.Removed;
+                case EventAction.Changed:
+                    return Action.Changed;
+            }
+            throw new NotImplementedException();
+        }
+
+		public static Action ToProtosAction(this NotifyDictionaryChangedAction action)
+        {
+            switch (action)
+            {
+                case NotifyDictionaryChangedAction.Added:
+                    return Action.Added;
+                case NotifyDictionaryChangedAction.Removed:
+                    return Action.Removed;
+                case NotifyDictionaryChangedAction.Changed:
+                    return Action.Changed;
+            }
+            throw new NotImplementedException();
+        }
+
+        public static EventAction ToEventAction(this Action action)
+        {
+            switch (action)
+            {
+                case Action.Added:
+                    return EventAction.Added;
+                case Action.Removed:
+                    return EventAction.Removed;
+                case Action.Changed:
+                    return EventAction.Changed;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
