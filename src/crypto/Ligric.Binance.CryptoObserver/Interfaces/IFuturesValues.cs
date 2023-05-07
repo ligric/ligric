4ce5a7822cc14a7dffcab0a81977ba716657c48c
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-07 13:06:39 +0300

    BinanceFuturesValues

---
+++ src/crypto/Ligric.Binance.CryptoObserver/Interfaces/IFuturesValues.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using Utils;
+
+namespace Ligric.CryptoObserver.Interfaces
+{
+	public interface IFuturesValues
+	{
+		ReadOnlyDictionary<string, decimal> Values { get; }
+
+		event EventHandler<NotifyDictionaryChangedEventArgs<string, decimal>>? ValuesChanged;
+	}
+}
