90be214697a0c864e18f4598c9206997390286d5
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-08 00:03:46 +0300

    Array extension  for and new value and shift elements

--- src/shared/Utils/Extensions/ArrayExtensions.cs
+++ src/shared/Utils/Extensions/ArrayExtensions.cs
@@ -2,15 +2,18 @@
 {
 	public static class ArrayExtensions
 	{
-		public static void PushWithShift<T>(this T[] array)
+		public static void PushWithShift<T>(this T[] array, T newItem)
 		{
 			if (array.Length == 0) return;
 
 			for (int i = 0; i < array.Length; i++)
 			{
 				array[i + 1] = array[i];
-				if (i == array.Length - 1) break;
+
+				if (i == array.Length - 2) break;
 			}
+
+			array[0] = newItem;
 		}
 	}
 }
