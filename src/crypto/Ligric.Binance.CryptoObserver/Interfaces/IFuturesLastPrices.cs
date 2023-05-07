209f5102135b6fa326b31555374d4f7b4e52d1fe
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-07 19:18:23 +0300

    IFuturesLastPrices

---
+++ src/crypto/Ligric.Binance.CryptoObserver/Interfaces/IFuturesLastPrices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using Utils;
+
+namespace Ligric.CryptoObserver.Interfaces
+{
+	public interface IFuturesLastPrices
+	{
+		ReadOnlyDictionary<string, decimal> LastValues { get; }
+
+		event EventHandler<NotifyDictionaryChangedEventArgs<string, decimal>>? LastValuesChanged;
+	}
+}
