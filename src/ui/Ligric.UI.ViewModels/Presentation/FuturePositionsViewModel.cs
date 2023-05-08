cde04713fb64e2893803876bdbfd71f5973e05c1
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-08 14:21:49 +0300

    ROE

--- src/ui/Ligric.UI.ViewModels/Presentation/FuturePositionsViewModel.cs
+++ src/ui/Ligric.UI.ViewModels/Presentation/FuturePositionsViewModel.cs
@@ -95,7 +95,7 @@ namespace Ligric.UI.ViewModels.Presentation
 							OpenPrice = oldValue.OpenPrice,
 							CurrentPrice = e.NewValue.ToString(),
 							PnL = CalculatePnL(oldValue.OpenPrice!, e.NewValue, oldValue.Quantity!),
-							PnLPercent = oldValue.PnLPercent,
+							PnLPercent = CalculateROE(oldValue.OpenPrice!, e.NewValue),
 						};
 					}
 				}
@@ -109,6 +109,14 @@ namespace Ligric.UI.ViewModels.Presentation
 				return "NaN";
 
 			return ((currentPrice - openPrice) * quantityUsdt).ToString();
+		}
+
+		private static string CalculateROE(string openPriceString, decimal currentPrice)
+		{
+			if (!decimal.TryParse(openPriceString, out decimal openPrice))
+				return "NaN";
+
+			return ((currentPrice - openPrice) / openPrice * 100).ToString();
 		}
 	}
 }
