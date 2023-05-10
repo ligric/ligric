0c3346ddc81ada773cd67ea5f8c7b0d3a50f0255
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-10 23:38:04 +0300

    Order SideRectangle color

--- src/ui/Ligric.UI/Views/FuturesPage.xaml.cs
+++ src/ui/Ligric.UI/Views/FuturesPage.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using System.Reactive.Linq;
 using Ligric.Core.Ligric.Core.Types.Api;
 using Ligric.UI.Helpers;
+using Ligric.UI.ViewModels.Data;
 using Ligric.UI.ViewModels.Presentation;
+using Microsoft.UI;
 
 namespace Ligric.UI.Views
 {
     public partial class FuturesPage : Page
     {
+		private static readonly SolidColorBrush
+			RED_COLOR = ToBrush("#FF5C5C"),
+			GREEN_COLOR = ToBrush("#5CFF94"),
+			SIMPLE_COLOR = new SolidColorBrush(Colors.White);
+
         public FuturesPage()
         {
             this.InitializeComponent();
@@ -53,6 +60,30 @@ namespace Ligric.UI.Views
 		private void OnApiCheckBoxUnchecked(object sender, RoutedEventArgs e)
 		{
 
+
+		public static Brush SideRectangleBrushFromOrderViewModel(OrderViewModel orderVm)
+			=> orderVm.Side is "Sell" ? RED_COLOR : GREEN_COLOR;
+
+		public static Brush SideTextBlockForegroundFromOrderViewModel(OrderViewModel orderVm)
+		{
+			bool isSell = orderVm.Side is "Sell";
+
+			return orderVm.Type switch
+			{
+				"TakeProfitMarket" => isSell ? GREEN_COLOR : RED_COLOR,
+				"StopMarket" => isSell ? GREEN_COLOR : RED_COLOR,
+				"Limit" => isSell ? GREEN_COLOR : RED_COLOR,
+				_ => SIMPLE_COLOR
+			};
+		}
+
+		private static SolidColorBrush ToBrush(string hex)
+		{
+			hex = hex.Replace("#", string.Empty);
+			byte r = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+			byte g = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+			byte b = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+			return new SolidColorBrush(Windows.UI.Color.FromArgb(255, r, g, b));
 		}
 	}
 }
