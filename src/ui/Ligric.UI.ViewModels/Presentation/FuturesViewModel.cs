6b626b3f4978d699e5dd58e3fbefc26d8205752e
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-16 05:55:55 +0300

    Logout command

--- src/ui/Ligric.UI.ViewModels/Presentation/FuturesViewModel.cs
+++ src/ui/Ligric.UI.ViewModels/Presentation/FuturesViewModel.cs
@@ -1,18 +1,24 @@
-﻿using Ligric.Business.Apies;
+﻿using System.Reactive;
+using Ligric.Business.Apies;
+using Ligric.Business.Authorization;
 using Ligric.Business.Futures;
+using ReactiveUI;
 
 namespace Ligric.UI.ViewModels.Presentation
 {
 	public class FuturesViewModel
 	{
+		private readonly IAuthorizationService _authorizationService;
 		public FuturesViewModel(
 			IDispatcher dispatcher,
 			IApiesService apiesService,
 			IOrdersService orderService,
 			IValuesService valuesService,
 			IPositionsService postionsService,
-			ILeveragesService leveragesService)
+			ILeveragesService leveragesService,
+			IAuthorizationService authorizationService)
 		{
+			_authorizationService = authorizationService;
 			Api = new ApisViewModel(dispatcher, apiesService, orderService, valuesService, postionsService, leveragesService);
 			FuturesOrders = new FutureOrdersViewModel(dispatcher, orderService, valuesService);
 			FuturePositions = new FuturePositionsViewModel(dispatcher, valuesService, postionsService, leveragesService);
@@ -23,5 +29,12 @@ namespace Ligric.UI.ViewModels.Presentation
 		public FutureOrdersViewModel FuturesOrders { get; }
 
 		public FuturePositionsViewModel FuturePositions { get; }
+
+		public ReactiveCommand<Unit, Unit> LogoutCommand => ReactiveCommand.Create(Logout);
+
+		private void Logout()
+		{
+			_authorizationService.Logout();
+		}
 	}
 }
