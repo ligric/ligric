aad38c65568e3e75a994054cc7cf429cdb156dbe
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-17 14:39:40 +0300

    SelectedApis

--- src/ui/Ligric.UI/Views/FuturesPage.xaml.cs
+++ src/ui/Ligric.UI/Views/FuturesPage.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Reactive.Linq;
-using Ligric.Core.Ligric.Core.Types.Api;
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
 using Ligric.UI.Helpers;
 using Ligric.UI.ViewModels.Data;
 using Ligric.UI.ViewModels.Presentation;
@@ -8,28 +8,69 @@ using Microsoft.UI;
 namespace Ligric.UI.Views
 {
 	public partial class FuturesPage : Page
-    {
+	{
 		private static readonly SolidColorBrush
 			RED_COLOR = ToBrush("#FF5C5C"),
 			GREEN_COLOR = ToBrush("#5CFF94"),
 			SIMPLE_COLOR = new SolidColorBrush(Colors.White);
 
+		private readonly ObservableCollection<ApiClientViewModel> _selectedApis = new ObservableCollection<ApiClientViewModel>();
+
 		public FuturesPage()
-        {
-            this.InitializeComponent();
+		{
+			this.InitializeComponent();
 			DataContextChanged += OnDataContextChanged;
-        }
+		}
+
+		public ReadOnlyObservableCollection<ApiClientViewModel> SelectedApis => new ReadOnlyObservableCollection<ApiClientViewModel>(_selectedApis);
 
 		public FuturesViewModel? ViewModel { get; private set; }
+
+		public static string GetOrderSideFromOrderViewModel(OrderViewModel orderVm)
+		{
+			const string closeShort = "Close Short", closeLong = "Close Long",
+						 openShort = "Open Short", openLong = "Open Long";
+
+			bool isSell = orderVm.Side is "Sell";
+
+			return orderVm.Type switch
+			{
+				"TakeProfitMarket" => isSell ? closeShort : closeLong,
+				"StopMarket" => isSell ? closeShort : closeLong,
+				"Limit" => isSell ? openShort : openLong,
+				_ => "-"
+			};
+		}
+
+		public static Brush SideRectangleBrushFromOrderViewModel(OrderViewModel orderVm)
+			=> orderVm.Side is "Sell" ? RED_COLOR : GREEN_COLOR;
+
+		public static Brush SideTextBlockForegroundFromOrderViewModel(OrderViewModel orderVm)
+		{
+			bool isSell = orderVm.Side is "Sell";
+
+			return orderVm.Type switch
+			{
+				"TakeProfitMarket" => isSell ? GREEN_COLOR : RED_COLOR,
+				"StopMarket" => isSell ? GREEN_COLOR : RED_COLOR,
+				"Limit" => isSell ? RED_COLOR : GREEN_COLOR,
+				_ => SIMPLE_COLOR
+			};
+		}
 
 		private void OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
 		{
 			ViewModel = args.NewValue as FuturesViewModel;
+			if (ViewModel != null)
+			{
+				ViewModel.Api.Apis.CollectionChanged -= OnCollectionChanged;
+				ViewModel.Api.Apis.CollectionChanged += OnCollectionChanged;
+			}
 		}
 
 		private async void ShareClick(object sender, RoutedEventArgs e)
 		{
-			if (ViewModel != null && e.OriginalSource is FrameworkElement ui && ui.DataContext is ApiClientDto api)
+			if (ViewModel != null && e.OriginalSource is FrameworkElement ui && ui.DataContext is ApiClientViewModel api)
 			{
 				await ViewModel.Api.ShareApiCommand.Execute(api);
 			}
@@ -51,20 +92,23 @@ namespace Ligric.UI.Views
 
 		private void OnApiCheckBoxChecked(object sender, RoutedEventArgs e)
 		{
-			if (ViewModel != null && e.OriginalSource is CheckBox apiCheckBox && apiCheckBox.DataContext is ApiClientDto api)
+			if (e.OriginalSource is ToggleButton apiToggleButton && apiToggleButton.DataContext is ApiClientViewModel api)
 			{
-				ViewModel.Api.AttachApiStreamsCommand.Execute(api);
+				_selectedApis.Add(api);
 			}
 		}
 
 		private void OnApiCheckBoxUnchecked(object sender, RoutedEventArgs e)
 		{
-
+			if (e.OriginalSource is ToggleButton apiToggleButton && apiToggleButton.DataContext is ApiClientViewModel api)
+			{
+				_selectedApis.Remove(api);
+			}
 		}
 
 		private void OnApiToggleButtonChecked(object sender, RoutedEventArgs e)
 		{
-			if (ViewModel != null && e.OriginalSource is ToggleButton apiToggleButton && apiToggleButton.DataContext is ApiClientDto api)
+			if (ViewModel != null && e.OriginalSource is ToggleButton apiToggleButton && apiToggleButton.DataContext is ApiClientViewModel api)
 			{
 				ViewModel.Api.AttachApiStreamsCommand.Execute(api);
 			}
@@ -72,43 +116,30 @@ namespace Ligric.UI.Views
 
 		private void OnApiToggleButtonUnchecked(object sender, RoutedEventArgs e)
 		{
-			if (ViewModel != null && e.OriginalSource is ToggleButton apiToggleButton && apiToggleButton.DataContext is ApiClientDto api)
+			if (ViewModel != null && e.OriginalSource is ToggleButton apiToggleButton && apiToggleButton.DataContext is ApiClientViewModel api)
 			{
 				ViewModel.Api.DetachApiStreamsCommand.Execute(api);
 			}
 		}
 
-		public static string GetOrderSideFromOrderViewModel(OrderViewModel orderVm)
+		private void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
 		{
-			const string closeShort = "Close Short", closeLong = "Close Long",
-						 openShort = "Open Short", openLong = "Open Long";
-
-			bool isSell = orderVm.Side is "Sell";
-			
-			return orderVm.Type switch
+			switch (e.Action)
 			{
-				"TakeProfitMarket" => isSell ? closeShort : closeLong,
-				"StopMarket" => isSell ? closeShort : closeLong,
-				"Limit" => isSell ? openShort : openLong,
-				_ => "-"
-			};
-		}
-
-
-		public static Brush SideRectangleBrushFromOrderViewModel(OrderViewModel orderVm)
-			=> orderVm.Side is "Sell" ? RED_COLOR : GREEN_COLOR;
-
-		public static Brush SideTextBlockForegroundFromOrderViewModel(OrderViewModel orderVm)
-		{
-			bool isSell = orderVm.Side is "Sell";
-
-			return orderVm.Type switch
-			{
-				"TakeProfitMarket" => isSell ? GREEN_COLOR : RED_COLOR,
-				"StopMarket" => isSell ? GREEN_COLOR : RED_COLOR,
-				"Limit" => isSell ? RED_COLOR : GREEN_COLOR,
-				_ => SIMPLE_COLOR
-			};
+				case NotifyCollectionChangedAction.Remove:
+					foreach (var oldItem in e.OldItems!)
+					{
+						var oldItemApi = oldItem as ApiClientViewModel;
+						if (_selectedApis.FirstOrDefault(x => x == oldItemApi) != null)
+						{
+							_selectedApis.Remove(oldItemApi!);
+						}
+					}
+					break;
+				case NotifyCollectionChangedAction.Reset:
+					_selectedApis.Clear();
+					break;
+			}
 		}
 
 		private static SolidColorBrush ToBrush(string hex)
