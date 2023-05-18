df9dd1b12e746c31ac68c54ea1b7acb23ad50b1c
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-18 22:50:24 +0300

    Hot fix

--- src/ui/Ligric.Business/Interfaces/Futures/IFuturesCryptoManager.cs
+++ src/ui/Ligric.Business/Interfaces/Futures/IFuturesCryptoManager.cs
@@ -1,9 +1,12 @@
-﻿using Utils;
+﻿using System.Collections.ObjectModel;
+using Utils;
 
 namespace Ligric.Business.Interfaces.Futures
 {
 	public interface IFuturesCryptoManager
 	{
+		public ReadOnlyDictionary<long, IFuturesCryptoClient> Clients { get; }
+
 		event EventHandler<NotifyDictionaryChangedEventArgs<long, IFuturesCryptoClient>>? ClientsChanged;
 
 		void AddFuturesCryptoClient(long userApiId);
