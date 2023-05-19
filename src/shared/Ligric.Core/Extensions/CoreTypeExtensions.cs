0feaff3daf7cf3686f2adbc2449c4ecef7a1fe03
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-20 00:08:22 +0300

    ToIdentityNotifyDictionaryChangedEventArgs

---
+++ src/shared/Ligric.Core/Extensions/CoreTypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Ligric.Core.Types;
+using Utils;
+
+namespace Ligric.Core.Extensions
+{
+	public static class CoreTypeExtensions
+	{
+		public static NotifyDictionaryChangedEventArgs<TKey, IdentityEntity<TValue>> ToIdentityNotifyDictionaryChangedEventArgs<TKey, TValue>(this NotifyDictionaryChangedEventArgs<TKey, TValue> args, Guid id)
+		{
+			return new NotifyDictionaryChangedEventArgs<TKey, IdentityEntity<TValue>>(args.Action, args.Key, new IdentityEntity<TValue>(id, args.OldValue), new IdentityEntity<TValue>(id, args.NewValue), args.Number, args.SenderTime);
+		}
+	}
+}
