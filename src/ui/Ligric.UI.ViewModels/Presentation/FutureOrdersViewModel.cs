856e257504e187f517f1058fee0706930e6c5534
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-19 16:54:32 +0300

    FuturesOrdersViewMolde with primary trades initialization

--- src/ui/Ligric.UI.ViewModels/Presentation/FutureOrdersViewModel.cs
+++ src/ui/Ligric.UI.ViewModels/Presentation/FutureOrdersViewModel.cs
@@ -47,46 +47,34 @@ namespace Ligric.UI.ViewModels.Presentation
 		}
 
 		private void OnOrdersChanged(object? sender, NotifyDictionaryChangedEventArgs<long, FuturesOrderDto> obj)
-		{
-			_dispatcher.TryEnqueue(() =>
-			{
-				UpdateOrdersFromAction(obj);
-			});
-		}
+			=> _dispatcher.TryEnqueue(() => UpdateOrdersFromAction(sender!, obj));
 
 		private void OnTradesChanged(object? sender, NotifyDictionaryChangedEventArgs<string, decimal> obj)
-		{
-			_dispatcher.TryEnqueue(() =>
-			{
-				UpdateOrdersFromAction(obj);
-			});
-		}
+			=> _dispatcher.TryEnqueue(() => UpdateOrdersFromAction(obj));
 
-		private void UpdateOrdersFromAction(NotifyDictionaryChangedEventArgs<long, FuturesOrderDto> obj)
+		private void UpdateOrdersFromAction(object sender, NotifyDictionaryChangedEventArgs<long, FuturesOrderDto> obj)
 		{
 			switch (obj.Action)
 			{
 				case NotifyDictionaryChangedAction.Added:
 					var addedOrder = obj.NewValue ?? throw new ArgumentException("Order is null");
-					Orders.Add(addedOrder.ToOrderViewModel());
+					var orderVm = addedOrder.ToOrderViewModel();
+					var client = GetClientFromOrderSender(sender)!;
+					if (client.Trades.Trades.TryGetValue(orderVm.Symbol!, out decimal value))
+					{
+						orderVm.CurrentPrice = value.ToString();
+					}
+					Orders.Add(orderVm);
 					break;
 				case NotifyDictionaryChangedAction.Removed:
 					var removedOrder = Orders.FirstOrDefault(x => x.Id == obj.Key.ToString());
-					if (removedOrder != null) Orders.Remove(removedOrder);
-					break;
-				case NotifyDictionaryChangedAction.Changed:
-					var changedOrder = obj.NewValue ?? throw new ArgumentException("Order is null");
-					var stringId = changedOrder.Id.ToString();
-					for (int i = 0; i < Orders.Count; i++)
+					if (removedOrder != null)
 					{
-						if (Orders[i].Id == stringId)
-						{
-							var changingItem = Orders[i];
-							changingItem.CurrentPrice = changedOrder.CurrentPrice?.ToString();
-							break;
-						}
+						Orders.Remove(removedOrder);
 					}
 					break;
+				case NotifyDictionaryChangedAction.Changed:
+					throw new NotImplementedException();
 				case NotifyDictionaryChangedAction.Cleared:
 					Orders.Clear();
 					break;
@@ -107,6 +95,11 @@ namespace Ligric.UI.ViewModels.Presentation
 					}
 				}
 			}
+		}
+
+		private IFuturesCryptoClient? GetClientFromOrderSender(object sender)
+		{
+			return _futuresCryptoManager.Clients.Values.FirstOrDefault(x => x.Orders == sender);
 		}
 	}
 }
