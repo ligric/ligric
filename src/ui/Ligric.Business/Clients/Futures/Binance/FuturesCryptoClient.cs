360f43e49c5fa050d0b497942b48493492c56cec
Mykyta Vtorenko <limeniye@gmail.com>
2023-05-20 00:10:41 +0300

    Added ClientId and Client events with ClientId

--- src/ui/Ligric.Business/Clients/Futures/Binance/FuturesCryptoClient.cs
+++ src/ui/Ligric.Business/Clients/Futures/Binance/FuturesCryptoClient.cs
@@ -2,6 +2,10 @@
 using Ligric.Business.Futures;
 using Ligric.Business.Interfaces;
 using Ligric.Business.Metadata;
+using Ligric.Core.Extensions;
+using Ligric.Core.Types;
+using Ligric.Core.Types.Future;
+using Utils;
 using static Ligric.Protobuf.Futures;
 
 namespace Ligric.Business.Clients.Futures.Binance
@@ -9,6 +13,7 @@ namespace Ligric.Business.Clients.Futures.Binance
 	public class FuturesCryptoClient : IFuturesCryptoClient
 	{
 		private readonly long userApi;
+		public Guid ClientId { get; }
 
 		private readonly FuturesOrdersService _orders;
 		private readonly FuturesTradesService _trades;
@@ -22,12 +27,17 @@ namespace Ligric.Business.Clients.Futures.Binance
 			long userApi)
 		{
 			this.userApi = userApi;
+			this.ClientId = Guid.NewGuid();
 
 			var futuresClient = new FuturesClient(channel);
 			_orders = new FuturesOrdersService(futuresClient, metadata, currentUser);
 			_trades = new FuturesTradesService(futuresClient, metadata, currentUser);
 			_positions = new FuturesPositionsService(futuresClient, metadata, currentUser);
 			_leverages = new FuturesLeveragesService(futuresClient, metadata, currentUser);
+
+			_orders.OrdersChanged += OnOrdersChanged;
+			_positions.PositionsChanged += OnPositionsChanged;
+			_leverages.LeveragesChanged += OnLeveragesChanged;
 		}
 
 		public IFuturesOrdersService Orders => _orders;
@@ -37,6 +47,21 @@ namespace Ligric.Business.Clients.Futures.Binance
 		public IFuturesPositionsService Positions => _positions;
 
 		public IFuturesLeveragesService Leverages => _leverages;
+
+		public event EventHandler<NotifyDictionaryChangedEventArgs<long, IdentityEntity<FuturesOrderDto>>>? ClientOrdersChanged;
+
+		public event EventHandler<NotifyDictionaryChangedEventArgs<long, IdentityEntity<FuturesPositionDto>>>? ClientPositionsChanged;
+
+		public event EventHandler<NotifyDictionaryChangedEventArgs<string, IdentityEntity<LeverageDto>>>? ClientLeveragesChanged;
+
+		private void OnOrdersChanged(object sender, NotifyDictionaryChangedEventArgs<long, FuturesOrderDto> e)
+			=> ClientOrdersChanged?.Invoke(this, e.ToIdentityNotifyDictionaryChangedEventArgs(ClientId));
+
+		private void OnPositionsChanged(object sender, NotifyDictionaryChangedEventArgs<long, FuturesPositionDto> e)
+			=> ClientPositionsChanged?.Invoke(this, e.ToIdentityNotifyDictionaryChangedEventArgs(ClientId));
+
+		private void OnLeveragesChanged(object sender, NotifyDictionaryChangedEventArgs<string, LeverageDto> e)
+			=> ClientLeveragesChanged?.Invoke(this, e.ToIdentityNotifyDictionaryChangedEventArgs(ClientId));
 
 		public async Task AttachStreamAsync()
 		{
@@ -56,6 +81,10 @@ namespace Ligric.Business.Clients.Futures.Binance
 
 		public void Dispose()
 		{
+			_orders.OrdersChanged -= OnOrdersChanged;
+			_positions.PositionsChanged -= OnPositionsChanged;
+			_leverages.LeveragesChanged -= OnLeveragesChanged;
+
 			Orders.Dispose();
 			Trades.Dispose();
 			Positions.Dispose();
--- src/ui/Ligric.Business/Interfaces/Futures/IFuturesCryptoClient.cs
+++ src/ui/Ligric.Business/Interfaces/Futures/IFuturesCryptoClient.cs
@@ -1,9 +1,14 @@
 ﻿using Ligric.Business.Futures;
+using Ligric.Core.Types;
+using Ligric.Core.Types.Future;
+using Utils;
 
 namespace Ligric.Business.Interfaces
 {
 	public interface IFuturesCryptoClient : IDisposable
 	{
+		Guid ClientId { get; }
+
 		IFuturesOrdersService Orders { get; }
 
 		IFuturesTradesService Trades { get; }
@@ -11,6 +16,12 @@ namespace Ligric.Business.Interfaces
 		IFuturesPositionsService Positions { get; }
 
 		IFuturesLeveragesService Leverages { get; }
+
+		event EventHandler<NotifyDictionaryChangedEventArgs<long, IdentityEntity<FuturesOrderDto>>>? ClientOrdersChanged;
+
+		event EventHandler<NotifyDictionaryChangedEventArgs<long, IdentityEntity<FuturesPositionDto>>>? ClientPositionsChanged;
+
+		event EventHandler<NotifyDictionaryChangedEventArgs<string, IdentityEntity<LeverageDto>>>? ClientLeveragesChanged;
 
 		Task AttachStreamAsync();
 
