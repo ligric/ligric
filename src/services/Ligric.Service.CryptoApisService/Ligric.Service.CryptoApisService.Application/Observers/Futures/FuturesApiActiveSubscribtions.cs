a22cb89cb0aefc870267781d4c179c9219b11809
Mykyta Vtorenko <limeniye@gmail.com>
2023-06-06 04:34:06 +0300

    FuturesApiActiveSubscribtions

---
+++ src/services/Ligric.Service.CryptoApisService/Ligric.Service.CryptoApisService.Application/Observers/Futures/FuturesApiActiveSubscribtions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Ligric.Service.CryptoApisService.Application.Observers.Futures
+{
+	public class FuturesApiActiveSubscribtions
+	{
+		private readonly Dictionary<Guid, long> _usersSubscriptions = new Dictionary<Guid, long>();
+
+		/// <remarks>
+		/// <c>Guid</c> key - subscription id<br/>
+		/// <c>long</c> value - user id
+		/// </remarks>
+		public ReadOnlyDictionary<Guid, long> UsersSubscribtions => new ReadOnlyDictionary<Guid, long>(_usersSubscriptions);
+
+		public void AttachSubscription(long userId, out Guid subscriptionId)
+		{
+			subscriptionId = Guid.NewGuid();
+			_usersSubscriptions.TryAdd(subscriptionId, userId);
+		}
+
+		public bool DetachSubscribtion(Guid userSubscribtion)
+		{
+			return _usersSubscriptions.Remove(userSubscribtion);
+		}
+	}
+}
