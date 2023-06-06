e31b97c7b20e4e0b37975d106f4fb2b036a79d08
Mykyta Vtorenko <limeniye@gmail.com>
2023-06-06 04:39:04 +0300

    Created ITradeSubscriptions and implemented in BinanceTradeSubscriptions

---
+++ src/services/Ligric.Service.CryptoApisService/Ligric.Service.CryptoApisService.Application/Observers/Futures/Burses/Binance/BinanceTradeSubscriptions.cs
@@ -0,0 +1,47 @@
+﻿using System.Reactive.Linq;
+using Ligric.Service.CryptoApisService.Application.Observers.Futures.Interfaces;
+using Ligric.Service.CryptoApisService.Application.Repositories;
+using Utils;
+using Ligric.Service.CryptoApisService.Domain.Extensions;
+
+namespace Ligric.Service.CryptoApisService.Application.Observers.Futures.Burses.Binance
+{
+	public class BinanceTradeSubscriptions : ITradeSubscriptions
+	{
+		private readonly IApiRepository _apiRepository;
+		private readonly BinanceFuturesApiSubscriptions _futuresApiSubscriptions;
+		private event Action<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, decimal> EventArgs)>? tradesChanged;
+
+		public BinanceTradeSubscriptions(
+			BinanceFuturesApiSubscriptions futuresApiSubscriptions,
+			IApiRepository apiRepository)
+		{
+			_apiRepository = apiRepository;
+			_futuresApiSubscriptions = futuresApiSubscriptions;
+		}
+
+		public IObservable<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, decimal> EventArgs)> GetTradesAsObservable()
+		{
+			var updatedApiStateNotifications = Observable.FromEvent<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, decimal> EventArgs)>((x)
+				=> tradesChanged += x, (x) => tradesChanged -= x);
+
+			return updatedApiStateNotifications;
+		}
+
+		public void SetSubscribedStream(long userApiId, long userId, out Guid subscribedStreamId)
+		{
+			var api = _apiRepository.GetEntityByUserApiId(userApiId).ToApiDto();
+
+			_futuresApiSubscriptions.AttachSubscriptionIdToApi(api, userId, out subscribedStreamId, out var burseSession);
+			burseSession.TradesChanged += OnTradesChanged;
+		}
+
+		public void UnsubscribeStream(Guid subscribedStreamId)
+		{
+			_futuresApiSubscriptions.DetachSubscriptionAndTryToRemoveApiSubscriptionObject(subscribedStreamId);
+		}
+
+		private void OnTradesChanged((Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, decimal> EventArgs) obj)
+			=> tradesChanged?.Invoke(obj);
+	}
+}
---
+++ src/services/Ligric.Service.CryptoApisService/Ligric.Service.CryptoApisService.Application/Observers/Futures/Interfaces/ITradeSubscriptions.cs
@@ -0,0 +1,13 @@
+﻿using Utils;
+
+namespace Ligric.Service.CryptoApisService.Application.Observers.Futures.Interfaces
+{
+	public interface ITradeSubscriptions
+	{
+		IObservable<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, decimal> EventArgs)> GetTradesAsObservable();
+
+		void SetSubscribedStream(long userApiId, long userId, out Guid subscribedStreamId);
+
+		void UnsubscribeStream(Guid subscribedStreamId);
+	}
+}
