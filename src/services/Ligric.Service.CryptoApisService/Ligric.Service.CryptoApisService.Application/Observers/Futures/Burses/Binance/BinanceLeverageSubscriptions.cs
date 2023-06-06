885063e71b4541bd4049a574684f27ee2209fe6a
Mykyta Vtorenko <limeniye@gmail.com>
2023-06-06 04:52:17 +0300

    Created ILeverageSubscriptions with BinanceLeverageSubscriptions implementation

---
+++ src/services/Ligric.Service.CryptoApisService/Ligric.Service.CryptoApisService.Application/Observers/Futures/Burses/Binance/BinanceLeverageSubscriptions.cs
@@ -0,0 +1,47 @@
+﻿using System.Reactive.Linq;
+using Ligric.Service.CryptoApisService.Application.Observers.Futures.Interfaces;
+using Ligric.Service.CryptoApisService.Application.Repositories;
+using Ligric.Service.CryptoApisService.Domain.Extensions;
+using Utils;
+
+namespace Ligric.Service.CryptoApisService.Application.Observers.Futures.Burses.Binance
+{
+	public class BinanceLeverageSubscriptions : ILeverageSubscriptions
+	{
+		private readonly IApiRepository _apiRepository;
+		private readonly BinanceFuturesApiSubscriptions _futuresApiSubscriptions;
+		private event Action<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, byte> EventArgs)>? laveragesChanged;
+
+		public BinanceLeverageSubscriptions(
+			BinanceFuturesApiSubscriptions futuresApiSubscriptions,
+			IApiRepository apiRepository)
+		{
+			_apiRepository = apiRepository;
+			_futuresApiSubscriptions = futuresApiSubscriptions;
+		}
+
+		public IObservable<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, byte> EventArgs)> GetLeveragesAsObservable()
+		{
+			var updatedApiStateNotifications = Observable.FromEvent<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, byte> EventArgs)>((x)
+				=> laveragesChanged += x, (x) => laveragesChanged -= x);
+
+			return updatedApiStateNotifications;
+		}
+
+		public void SetSubscribedStream(long userApiId, long userId, out Guid subscribedStreamId)
+		{
+			var api = _apiRepository.GetEntityByUserApiId(userApiId).ToApiDto();
+
+			_futuresApiSubscriptions.AttachSubscriptionIdToApi(api, userId, out subscribedStreamId, out var burseSession);
+			burseSession.LeveragesChanged += OnLeveragesChanged;
+		}
+
+		public void UnsubscribeStream(Guid subscribedStreamId)
+		{
+			_futuresApiSubscriptions.DetachSubscriptionAndTryToRemoveApiSubscriptionObject(subscribedStreamId);
+		}
+
+		private void OnLeveragesChanged((Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, byte> EventArgs) obj)
+			=> laveragesChanged?.Invoke(obj);
+	}
+}
---
+++ src/services/Ligric.Service.CryptoApisService/Ligric.Service.CryptoApisService.Application/Observers/Futures/Interfaces/ILeverageSubscriptions.cs
@@ -0,0 +1,13 @@
+﻿using Utils;
+
+namespace Ligric.Service.CryptoApisService.Application.Observers.Futures.Interfaces
+{
+	public interface ILeverageSubscriptions
+	{
+		IObservable<(Guid ExchangeId, NotifyDictionaryChangedEventArgs<string, byte> EventArgs)> GetLeveragesAsObservable();
+
+		void SetSubscribedStream(long userApiId, long userId, out Guid subscribedStreamId);
+
+		void UnsubscribeStream(Guid subscribedStreamId);
+	}
+}
