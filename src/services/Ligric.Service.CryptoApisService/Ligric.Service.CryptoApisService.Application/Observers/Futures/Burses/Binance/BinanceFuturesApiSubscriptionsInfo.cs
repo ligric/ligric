cb578687f41807216c7ef943a29695504f4302a8
Mykyta Vtorenko <limeniye@gmail.com>
2023-06-06 04:23:18 +0300

    Created BinanceFuturesApiSubscriptionsInfo

---
+++ src/services/Ligric.Service.CryptoApisService/Ligric.Service.CryptoApisService.Application/Observers/Futures/Burses/Binance/BinanceFuturesApiSubscriptionsInfo.cs
@@ -0,0 +1,19 @@
+﻿using Binance.Net.Objects;
+using Ligric.Core.Ligric.Core.Types.Api;
+using Ligric.Service.CryptoApisService.Application.Observers.Futures.Interfaces;
+
+namespace Ligric.Service.CryptoApisService.Application.Observers.Futures.Burses.Binance
+{
+	public class BinanceFuturesApiSubscriptionsInfo
+	{
+		public BinanceFuturesApiSubscriptionsInfo(ApiDto api, BinanceApiCredentials credentials, bool isTest = true)
+		{
+			FuturesBurseClient = new BinanceFuturesApiSubscriptionsBurseSessionWrapper(api, credentials, isTest);
+			ActiveSubscribtions = new FuturesApiActiveSubscribtions();
+		}
+
+		public IFuturesApiSubscriptionsBurseSessionWrapper FuturesBurseClient { get; }
+		 
+		public FuturesApiActiveSubscribtions ActiveSubscribtions { get; }
+	}
+}
